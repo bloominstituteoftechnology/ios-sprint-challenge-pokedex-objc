2520819a3262079b6a7c726d9b274a7aea85cada
Linh Bouniol <paulalinh89@gmail.com>
2018-10-12 08:13:37 -0700

    Add storyboards

---
+++ Pokedex/Pokedex/View Controllers/LTBPokemonTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  LTBPokemonTableViewController.h
+//  Pokedex
+//
+//  Created by Linh Bouniol on 10/12/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LTBPokemonTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/Pokedex/View Controllers/PokemonDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PokemonDetailViewController.swift
+//  Pokedex
+//
+//  Created by Linh Bouniol on 10/12/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
