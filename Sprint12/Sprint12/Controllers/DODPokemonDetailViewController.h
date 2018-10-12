4afe5fb3ed237086e8492d783b5a2ec4795acc7e
David O. Doswell <daviddoswell@lambda-school-loaner-11s-macbook-pro.local>
2018-10-12 11:12:44 -0700

    set up skeleton for client

--- Sprint12/Sprint12/Controllers/DODPokemonDetailViewController.h
+++ Sprint12/Sprint12/Controllers/DODPokemonDetailViewController.h
@@ -13,7 +13,6 @@ NS_ASSUME_NONNULL_BEGIN
 @interface DODPokemonDetailViewController : UIViewController
 
 
-
 @end
 
 NS_ASSUME_NONNULL_END
--- Sprint12/Sprint12/Networking/PokemonClient.swift
+++ Sprint12/Sprint12/Networking/PokemonClient.swift
@@ -13,26 +13,40 @@ class PokemonAPI: NSObject {
     
     let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
     
+    // TODO: How do we use this shared instance?
+
 //    @objc(sharedController) static let shared: PokemonAPI
+    
+    private var pokemons: [DODPokemon] = []
     
     @objc func fetchAllPokemon(completion: @escaping ([DODPokemon]?, Error?) -> Void) {
         
         let request = URLRequest(url: baseURL)
         
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error decoding data: \(error)")
+            }
+            guard let data = data else { return }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any]
+                
+                // TODO: How do we return this?
+                
+                print(json as Any)
+            } catch {
+                NSLog("Error deserialziing JSON: \(error)")
+            }
+            
+        }
+        
     }
     
-    var name: String?
-    var identifier: String?
-    var sprite: String?
-    var abilities: String?
-    
-    @objc func fillInDetails(for pokemon: DODPokemon) {
+    @objc func fillInDetails(for pokemon: DODPokemon, name: String, identifier: String, sprite: String, abilities: String) {
         
-        self.name = pokemon.name
-        self.identifier = pokemon.identifier
-        self.sprite = pokemon.sprite
-        self.abilities = pokemon.abilities
-        
+        let pokemon = DODPokemon(name: name, identifier: identifier, sprite: sprite, abilities: abilities)
+        pokemons.append(pokemon)
     }
     
 }
