31a55f36f673dd2be71bdf9435eff9cbd54967f0
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-30 12:03:43 -0600

    fillInDetails and fetchPokemonImage completed

--- Pokedex Objc Swift/Pokedex Objc Swift/PokemonController.swift
+++ Pokedex Objc Swift/Pokedex Objc Swift/PokemonController.swift
@@ -20,25 +20,26 @@ class PokemonController: NSObject {
         
         URLSession.shared.dataTask(with: request) { (data, _, error) in
             if let error = error {
-                NSLog("Error fetching pokedex: \(error)")
+                NSLog("fetchAllPokemon: Error fetching pokedex: \(error)")
                 completion(nil, error)
                 return
             }
             
             guard let data = data else {
+                NSLog("fetchAllPokemon: No data returned from Url Session")
                 completion(nil, NSError())
                 return
             }
             
             do {
                 guard let results = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary else {
-                    NSLog("Json object is not an NSDictionary")
+                    NSLog("fetchAllPokemon: Json object is not an NSDictionary")
                     completion(nil, NSError())
                     return
                 }
                 
                 guard let pokedexDictionary = results["results"] as? NSArray else {
-                    NSLog("Pokedex dictionary results is not an NSArray")
+                    NSLog("fetchAllPokemon: Pokedex dictionary results is not an NSArray")
                     completion(nil, NSError())
                     return
                 }
@@ -54,7 +55,7 @@ class PokemonController: NSObject {
                     completion(pokedex, nil)
                 }
             } catch {
-                NSLog("Unable encode json data: \(error)")
+                NSLog("fetchAllPokemon: Unable encode json data: \(error)")
                 completion(nil, error)
                 return
             }
@@ -62,10 +63,59 @@ class PokemonController: NSObject {
     }
     
     @objc func fillInDetails(for pokemon: IIIPokemon) {
+        let url = PokemonController.baseURL.appendingPathComponent(pokemon.pokemonName)
         
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("fillInDetails: Error fetching pokedex: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("fillInDetails: No data returned from Url Session")
+                return
+            }
+            
+            do {
+                guard let pokemonDictionary = try JSONSerialization.jsonObject(with: data, options: []) as? [AnyHashable : Any] else {
+                    NSLog("fillInDetails: Json object is not an NSDictionary")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    pokemon.update(with: pokemonDictionary)
+                }
+            } catch {
+                NSLog("fillInDetails: Unable encode json data: \(error)")
+                return
+            }
+        }.resume()
     }
     
-    @objc func fetchImage(url: URL, completion: @escaping (UIImage?, Error?) -> Void) {
+    @objc func fetchPokemonImage(url: URL, completion: @escaping (UIImage?, Error?) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
         
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("fetchPokemonImage: Error fetching pokedex: \(error)")
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("fetchPokemonImage: No data returned from Url Session")
+                completion(nil, NSError())
+                return
+            }
+            
+            let image = UIImage(data: data)
+            DispatchQueue.main.async {
+                completion(image, nil)
+            }
+        }.resume()
     }
 }
