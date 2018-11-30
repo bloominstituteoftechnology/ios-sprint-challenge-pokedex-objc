d26aec1dbbca2a85076a3d379830e92f7e812fc1
Scott Bennett <stt.btt@gmail.com>
2018-11-30 11:23:53 -0500

    Add UI

---
+++ Pokedex ObjC/Pokedex ObjC/Models/SLBPokemon.m
@@ -0,0 +1,13 @@
+//
+//  SLBPokemon.m
+//  Pokedex ObjC
+//
+//  Created by Scott Bennett on 11/30/18.
+//  Copyright © 2018 Scott Bennett. All rights reserved.
+//
+
+#import "SLBPokemon.h"
+
+@implementation SLBPokemon
+
+@end
---
+++ Pokedex ObjC/Pokedex ObjC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokedex ObjC
+//
+//  Created by Scott Bennett on 11/30/18.
+//  Copyright © 2018 Scott Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
