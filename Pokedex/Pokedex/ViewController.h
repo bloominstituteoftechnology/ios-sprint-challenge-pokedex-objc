04550cf79b3d2511ef594b9382316d9c492b3757
OmegaWulf <nikcxz@gmail.com>
2019-02-01 11:18:17 -0500

    initial

---
+++ Pokedex/Pokedex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pokedex
+//
+//  Created by Nikita Thomas on 2/1/19.
+//  Copyright © 2019 Nikita Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pokedex/Pokedex/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Pokedex
+//
+//  Created by Nikita Thomas on 2/1/19.
+//  Copyright © 2019 Nikita Thomas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
