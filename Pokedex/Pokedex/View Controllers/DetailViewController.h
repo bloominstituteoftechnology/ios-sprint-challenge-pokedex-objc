d8e5e3b5e005db337ab5ceb25bd9718f0772ba81
OmegaWulf <nikcxz@gmail.com>
2019-02-01 12:06:50 -0500

    TableView

--- Pokedex/Pokedex/View Controllers/DetailViewController.h
+++ Pokedex/Pokedex/View Controllers/DetailViewController.h
@@ -7,10 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "NKTPokemon.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DetailViewController : UIViewController
+
+@property (nonatomic, weak) NKTPokemon *pokemon;
 
 @end
 
--- Pokedex/Pokedex/View Controllers/NKTListTableViewController.m
+++ Pokedex/Pokedex/View Controllers/NKTListTableViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "NKTListTableViewController.h"
+#import "NKTPokemon.h"
+#import "Pokedex-Swift.h"
+#import "DetailViewController.h"
 
 @interface NKTListTableViewController ()
 
@@ -14,80 +17,67 @@
 
 @implementation NKTListTableViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
+-(instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        [[PokemonAPI shared] fetchPokemonWithCompletion:^(NSArray<NKTPokemon *> * fetchedPokemon, NSError * error)
+         {
+             PokemonAPI.shared.pokemon = fetchedPokemon;
+         }];
+    }
+    return self;
 }
+
+
+- (void)viewDidLoad
+{
+    [[PokemonAPI shared] fetchPokemonWithCompletion:^(NSArray<NKTPokemon *> * fetchedPokemon, NSError * error)
+     {
+         PokemonAPI.shared.pokemon = fetchedPokemon;
+         dispatch_async(dispatch_get_main_queue(),^{
+             [[self tableView] reloadData];
+         });
+     }];
+}
+
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return PokemonAPI.shared.pokemon.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    NKTPokemon *pokemon = [[[PokemonAPI shared] pokemon] objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = pokemon.name;
     
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier]isEqualToString:@"toDetail"])
+    {
+        DetailViewController *dest = segue.destinationViewController;
+        NSIndexPath *selectedIndex = [[self tableView] indexPathForSelectedRow];
+        
+        NKTPokemon *pokemon = [[[PokemonAPI shared] pokemon] objectAtIndex:selectedIndex.row];
+        
+        dest.pokemon = pokemon;
+        
+    }
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
 
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
