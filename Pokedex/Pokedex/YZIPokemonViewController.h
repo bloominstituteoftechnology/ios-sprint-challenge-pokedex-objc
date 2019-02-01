3022453a94ea90a61cbce760762f5fd4cad1c777
yzhukovs <yzhukovs@bumnetworks.com>
2019-02-01 11:43:32 -0500

    added UI

---
+++ Pokedex/Pokedex/YZIPokemonViewController.h
@@ -0,0 +1,17 @@
+//
+//  PokemonViewController.h
+//  Pokedex
+//
+//  Created by Yvette Zhukovsky on 2/1/19.
+//  Copyright © 2019 Yvette Zhukovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YZIPokemonViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/Pokedex/YZIPokemonViewController.m
@@ -0,0 +1,37 @@
+//
+//  PokemonViewController.m
+//  Pokedex
+//
+//  Created by Yvette Zhukovsky on 2/1/19.
+//  Copyright © 2019 Yvette Zhukovsky. All rights reserved.
+//
+
+#import "YZIPokemonViewController.h"
+
+@interface YZIPokemonViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *idLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *abilitiesLabel;
+
+@end
+
+@implementation YZIPokemonViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
