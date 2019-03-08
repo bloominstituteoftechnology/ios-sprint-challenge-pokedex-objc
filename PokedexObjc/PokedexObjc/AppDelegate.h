dee2462e0ece6190d6d705b840de5c87e4e2e56f
Dillon McElhinney <dillon.mce@gmail.com>
2019-03-08 09:05:41 -0700

    Initial commit
    Add empty Xcode project

---
+++ PokedexObjc/PokedexObjc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokedexObjc
+//
+//  Created by Dillon McElhinney on 3/8/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
