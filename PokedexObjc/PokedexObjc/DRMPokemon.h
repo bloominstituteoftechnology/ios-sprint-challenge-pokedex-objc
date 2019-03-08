cc95b482d4be3a5700648b6fcc44a6ce8b0562c9
Dillon McElhinney <dillon.mce@gmail.com>
2019-03-08 09:25:42 -0700

    Add pokemon model

---
+++ PokedexObjc/PokedexObjc/DRMPokemon.h
@@ -0,0 +1,25 @@
+//
+//  DRMPokemon.h
+//  PokedexObjc
+//
+//  Created by Dillon McElhinney on 3/8/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DRMPokemon : NSObject
+
+@property (readonly, copy) NSString *name;
+@property (readonly, copy) NSURL *url;
+@property (nullable, copy) NSString *identifier;
+@property (nullable, copy) NSURL *spriteURL;
+@property (nullable, copy) NSArray<NSString *> *abilities;
+
+- (instancetype)initWithDictionary: (NSDictionary<NSString *, NSString *> *)dictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PokedexObjc/PokedexObjc/DRMPokemon.m
@@ -0,0 +1,30 @@
+//
+//  DRMPokemon.m
+//  PokedexObjc
+//
+//  Created by Dillon McElhinney on 3/8/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+#import "DRMPokemon.h"
+
+@implementation DRMPokemon
+
+- (instancetype)initWithName: (NSString *)name andURL: (NSURL *)url {
+    self = [super init];
+    if (self != nil) {
+        _name = name;
+        _url = url;
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary<NSString *,NSString *> *)dictionary {
+    NSString *name = dictionary[@"name"];
+    NSString *urlString = dictionary[@"url"];
+    NSURL *url = [NSURL URLWithString: urlString];
+    
+    return [self initWithName: name andURL: url];
+}
+
+@end
