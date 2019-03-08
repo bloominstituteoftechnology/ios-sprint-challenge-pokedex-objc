1dd1d62e8558f2dfb8d6de8ba00ef25435acc7aa
Dillon McElhinney <dillon.mce@gmail.com>
2019-03-08 09:14:18 -0700

    Layout views and add custom view controllers

---
+++ PokedexObjc/PokedexObjc/PokedexTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  PokedexTableViewController.h
+//  PokedexObjc
+//
+//  Created by Dillon McElhinney on 3/8/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PokedexTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PokedexObjc/PokedexObjc/PokemonDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  PokemonDetailViewController.h
+//  PokedexObjc
+//
+//  Created by Dillon McElhinney on 3/8/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PokemonDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
