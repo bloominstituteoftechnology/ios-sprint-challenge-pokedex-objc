cedc0901b6e2864eb48fd71f52f231013a968d2c
Sean Hendrix <hendoware@gmail.com>
2019-03-08 12:31:01 -0500

    fixed detail view controller

---
+++ Pokedex/Pokedex/SHPokemonTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  SHPokemonTableTableViewController.h
+//  Pokedex
+//
+//  Created by Sean Hendrix on 3/8/19.
+//  Copyright © 2019 Sean Hendrix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SHPokemonTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
