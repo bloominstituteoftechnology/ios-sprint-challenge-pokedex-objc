e0ebf5d255096d2167342e09b32f53cbf88fa435
iyinraphael <iyinraphael@gmail.com>
2019-03-15 10:43:33 -0700

    detail view logic complete

--- Pokedex-ObjC/Pokedex-ObjC/View Controller/LSIPokemonViewController.m
+++ Pokedex-ObjC/Pokedex-ObjC/View Controller/LSIPokemonViewController.m
@@ -7,12 +7,17 @@
 //
 
 #import "LSIPokemonViewController.h"
+#import "LSIPokemon.h"
+#import "Pokedex_ObjC-Swift.h"
+
 
 @interface LSIPokemonViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
-@property (weak, nonatomic) IBOutlet UIStackView *idLabel;
+@property (weak, nonatomic) IBOutlet UILabel *idLabel;
 @property (weak, nonatomic) IBOutlet UILabel *abilitiesLabel;
+
+@property PokemonGET *network;
 
 @end
 
@@ -20,8 +25,48 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.pokemon addObserver:self forKeyPath:@"sprites" options:0 context:NULL];
+    [self updateView];
+    [self.network fetchDetailsFor:self.pokemon];
 
 }
 
+-(void) updateView {
+    if (![self.pokemon name]) {
+        return;
+    }
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self setTitle:self.pokemon.name];
+        self.nameLabel.text = self.pokemon.name;
+        self.abilitiesLabel.text = [self.pokemon abilities];
+        self.idLabel.text =[self.pokemon id];
+        [self loadImageWithString: [self.pokemon sprites]];
+    });
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context
+{
+    
+    [self updateView];
+    NSLog(@"ID %@", [object valueForKeyPath:@"abilities"]);
+    
+}
+
+
+-(void) loadImageWithString: (NSString *) fileName{
+    
+    NSURL *url = [NSURL URLWithString: fileName];
+    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if(error){
+            NSLog(@"Error with image loading: %@", error);
+        }
+        UIImage *image = [[UIImage alloc] initWithData:data];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[self imageView] setImage:image];
+        });
+        
+    }] resume];
+    
+}
 
 @end
