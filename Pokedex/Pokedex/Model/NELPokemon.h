883c97e8572e18fc5980f17ac4c75b8bbcf94217
Nelson Gonzalez <nelglezfl@gmail.com>
2019-04-05 07:24:29 -0400

    Add Model

--- Pokedex/Pokedex/Model/NELPokemon.h
+++ Pokedex/Pokedex/Model/NELPokemon.h
@@ -12,6 +12,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NELPokemon : NSObject
 
+-(instancetype)initWithDictionary:(NSDictionary *)dictionary;
+-(void)fillPokeDictionary:(NSDictionary *)dictionary;
+
+@property (nonatomic, copy, readonly) NSString *pokeName;
+@property (nonatomic, copy, readonly) NSURL *pokeUrl;
+@property (nonatomic, readonly) NSNumber *pokeId;
+@property (nonatomic, copy, readonly, nullable) NSURL *pokeFrontDefaultUrl;
+@property (nonatomic, copy, nullable) NSArray<NSString *> *pokeAbilities;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Pokedex/Pokedex/Model/NELPokemon.m
+++ Pokedex/Pokedex/Model/NELPokemon.m
@@ -8,6 +8,42 @@
 
 #import "NELPokemon.h"
 
-@implementation NELPokemon
+@interface NELPokemon ()
+
+@property (nonatomic, nullable, copy, readwrite) NSMutableArray<NSString *> *internalAbilities;
 
 @end
+
+@implementation NELPokemon
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    self = [super init];
+    if (self) {
+        _pokeName = dictionary[@"name"];
+        _pokeUrl = dictionary[@"url"];
+        _pokeId = nil;
+        _pokeFrontDefaultUrl = nil;
+        _pokeAbilities = nil;
+        
+    }
+    return self;
+}
+
+- (void)fillPokeDictionary:(NSDictionary *)dictionary
+{
+    _pokeId = dictionary[@"id"];
+    
+    NSDictionary *spritesDict = dictionary[@"sprites"];
+    _pokeFrontDefaultUrl = spritesDict[@"font_default"];
+    
+    NSArray *abilities = dictionary[@"abilities"];
+    for (NSDictionary *ability in abilities) {
+        NSString *name = ability[@"name"];
+        [self.internalAbilities addObject: name];
+    }
+    _pokeAbilities = self.internalAbilities;
+}
+
+@end
+
