bb9262d3bdfc11a322e8c3fc37460b85d9b043aa
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-04-12 11:54:02 -0400

    model is complete

--- Pokedex/Pokedex/GLIPokemon.h
+++ Pokedex/Pokedex/GLIPokemon.h
@@ -12,6 +12,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GLIPokemon : NSObject
 
+- (instancetype)initWithName:(NSString *)name  id:(int)identifier spritesURL:(NSString *)spritesURL abilities:(NSString *)abilities;
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+
+@property (nonatomic, copy, readonly) NSString *name;
+@property (nonatomic, readonly) int identifier;
+@property (nonatomic, copy, readonly) NSString *spritesURL;
+@property (nonatomic, copy, readonly) NSString *abilities;
+
 @end
 
 NS_ASSUME_NONNULL_END
