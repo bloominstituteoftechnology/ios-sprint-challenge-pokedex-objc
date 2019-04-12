507f4fd19d229ca61aa995977760839082bb9c73
Frulwinn <frulwinn@gmail.com>
2019-04-12 09:59:58 -0500

    initial commit

---
+++ Pokedex/Pokedex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokedex
+//
+//  Created by Lambda_School_Loaner_34 on 4/12/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pokedex/Pokedex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pokedex
+//
+//  Created by Lambda_School_Loaner_34 on 4/12/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
