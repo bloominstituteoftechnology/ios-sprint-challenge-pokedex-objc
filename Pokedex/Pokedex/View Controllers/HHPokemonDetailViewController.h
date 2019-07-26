f2a3f9ef77543a463e93e2641cd8fd3bd0fb1a4e
Hayden Hastings <haydenhastings32@gmail.com>
2019-07-26 10:00:55 -0600

    Finished the Table View Controller to load all the pokémon's .

---
+++ Pokedex/Pokedex/View Controllers/HHPokemonDetailViewController.h
@@ -0,0 +1,21 @@
+//
+//  HHPokemonDetailViewController.h
+//  Pokedex
+//
+//  Created by Hayden Hastings on 7/26/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HHPokemon;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HHPokemonDetailViewController : UIViewController
+
+@property HHPokemon *pokemon;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/Pokedex/View Controllers/HHPokemonDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  HHPokemonDetailViewController.m
+//  Pokedex
+//
+//  Created by Hayden Hastings on 7/26/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+#import "HHPokemonDetailViewController.h"
+
+@interface HHPokemonDetailViewController ()
+
+@end
+
+@implementation HHPokemonDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
