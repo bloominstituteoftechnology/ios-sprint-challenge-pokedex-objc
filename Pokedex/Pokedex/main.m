d3589715d4f3ca71a5d89785da262b9c527a9ce2
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-26 08:10:37 -0700

    init

---
+++ Pokedex/Pokedex/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pokedex
+//
+//  Created by Hector Steven on 7/26/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
