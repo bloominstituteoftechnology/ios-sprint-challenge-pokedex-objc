c054b93c5e57702bcd656fdb4ea2cc82a81dbcd2
Christopher Aronson <christopheraronson@icloud.com>
2019-07-26 08:10:41 -0700

    Initial Commit

---
+++ Pokemon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokemon
+//
+//  Created by Christopher Aronson on 7/26/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pokemon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pokemon
+//
+//  Created by Christopher Aronson on 7/26/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
