5bc29f7cd8439417d3e9ed113b0e549d38662268
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-26 08:29:24 -0700

    model

---
+++ Pokedex/Pokedex/Models/HSVPokemon.h
@@ -0,0 +1,27 @@
+//
+//  HSVPokemon.h
+//  Pokedex
+//
+//  Created by Hector Steven on 7/26/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HSVPokemon : NSObject
+
+
+@property (nonatomic, nullable, copy) NSString *name;
+@property (nonatomic, nullable, copy) NSString *identifier;
+
+@property (nonatomic, nullable, readwrite,copy) NSString *sprite;
+@property (nonatomic, nullable, readwrite,copy) NSArray<NSString *> *abilities;
+
+- (instancetype)initWithName:(NSString *)name identifier:(NSString *)identifier;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
