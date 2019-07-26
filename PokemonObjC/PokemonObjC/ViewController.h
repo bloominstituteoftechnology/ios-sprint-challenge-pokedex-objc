a74a816d26ecde152e0f450603783d5a69295fe1
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-07-26 11:15:37 -0400

    initial commit

---
+++ PokemonObjC/PokemonObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PokemonObjC
+//
+//  Created by Ryan Murphy on 7/26/19.
+//  Copyright © 2019 Ryan Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PokemonObjC/PokemonObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PokemonObjC
+//
+//  Created by Ryan Murphy on 7/26/19.
+//  Copyright © 2019 Ryan Murphy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
