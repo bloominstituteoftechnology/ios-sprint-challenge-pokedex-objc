2f1217c6c776a47a24c44e7cd73f343c3ba2fe93
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-26 11:41:46 -0600

    Clean code and optimize

--- PokedexObjC/PokedexObjC/Model/MBPokemon.h
+++ PokedexObjC/PokedexObjC/Model/MBPokemon.h
@@ -13,18 +13,12 @@ NS_ASSUME_NONNULL_BEGIN
 NS_SWIFT_NAME(Pokemon)
 @interface MBPokemon : NSObject
 
--(instancetype)initWithName:(NSString *)name sprite:(NSURL *)sprite idNumber:(NSNumber *)idNumber abilities:(NSArray *)abilities;
-
 @property (nonatomic, copy, readwrite) NSString *name;
 @property (nonatomic, copy, readwrite, nullable) NSURL *sprite;
 @property (nonatomic, copy, readwrite, nullable) NSNumber *idNumber;
 @property (nonatomic, copy, readwrite, nullable) NSArray<NSString *> *abilities;
 
-@end
-
-
-@interface MBPokemon (JSONSerialization)
-
+-(instancetype)initWithName:(NSString *)name sprite:(NSURL *)sprite idNumber:(NSNumber *)idNumber abilities:(NSArray *)abilities;
 -(instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
 @end
