28cd68653a26fd11c2d46bf786719204f04b76b3
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-26 08:18:17 -0700

    HSVPokedexTableViewController, HSVPokemonViewController with segue

---
+++ Pokedex/Pokedex/View Controllers/HSVPokemonViewController.h
@@ -0,0 +1,17 @@
+//
+//  HSVPokemonViewController.h
+//  Pokedex
+//
+//  Created by Hector Steven on 7/26/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HSVPokemonViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/Pokedex/View Controllers/HSVPokemonViewController.m
@@ -0,0 +1,32 @@
+//
+//  HSVPokemonViewController.m
+//  Pokedex
+//
+//  Created by Hector Steven on 7/26/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import "HSVPokemonViewController.h"
+
+@interface HSVPokemonViewController ()
+
+@end
+
+@implementation HSVPokemonViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
