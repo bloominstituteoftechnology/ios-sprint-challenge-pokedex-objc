8cfb1920b0d8fc1937909d93e17716960a58ba01
dllewisjolley <dlewisjolley@gmail.com>
2019-07-26 11:40:58 -0400

    initial Commit

---
+++ ObjcPokemon/ObjcPokemon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjcPokemon
+//
+//  Created by Diante Lewis-Jolley on 7/26/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObjcPokemon/ObjcPokemon/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ObjcPokemon
+//
+//  Created by Diante Lewis-Jolley on 7/26/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
