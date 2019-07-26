e4c3fe926db08ce96b838049dc5bde5b10b67224
Christopher Aronson <christopheraronson@icloud.com>
2019-07-26 08:31:58 -0700

    Created View Controllers

---
+++ Pokemon/View Controllers/KRCPokemonDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  KRCPokemonDetailViewController.m
+//  Pokemon
+//
+//  Created by Christopher Aronson on 7/26/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import "KRCPokemonDetailViewController.h"
+
+@interface KRCPokemonDetailViewController ()
+
+@end
+
+@implementation KRCPokemonDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Pokemon/View Controllers/KRCPokemonTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  KRCPokemonTableViewController.h
+//  Pokemon
+//
+//  Created by Christopher Aronson on 7/26/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KRCPokemonTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
