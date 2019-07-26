0efb9a0fb1ec40c6a8fe02b0aaf350329ff16c9e
Christopher Aronson <christopheraronson@icloud.com>
2019-07-26 09:13:43 -0700

    Created allPokemon array for table view controller

--- Pokemon/View Controllers/KRCPokemonTableViewController.m
+++ Pokemon/View Controllers/KRCPokemonTableViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "KRCPokemonTableViewController.h"
+#import "Pokemon-Swift.h"
+#import "KRCPokemon.h"
 
 @interface KRCPokemonTableViewController ()
+
+@property (nonatomic, nullable) NSArray<KRCPokemon *> *allPokemon;
 
 @end
 
@@ -17,22 +21,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
     return 0;
 }
 
