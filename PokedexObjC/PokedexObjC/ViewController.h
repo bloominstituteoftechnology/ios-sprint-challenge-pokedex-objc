8189c7b0b17edc50ba18dc169e61a0f07f84c056
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-26 09:02:57 -0600

    Initial commit with new Xcode project

---
+++ PokedexObjC/PokedexObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PokedexObjC
+//
+//  Created by Mitchell Budge on 7/26/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PokedexObjC/PokedexObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PokedexObjC
+//
+//  Created by Mitchell Budge on 7/26/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
