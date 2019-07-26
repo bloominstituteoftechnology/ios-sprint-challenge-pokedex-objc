3cd14e130f6a5c44b22071ec79bc01ab15fcb83a
Sameera Leola <pixelandbyte@gmail.com>
2019-07-26 10:29:05 -0500

    Initial commit. Created project and added table views

---
+++ Pokedex/Pokedex/View Controllers/SLRDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  SLRDetailViewController.h
+//  Pokedex
+//
+//  Created by Sameera Roussi on 7/26/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLRDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
