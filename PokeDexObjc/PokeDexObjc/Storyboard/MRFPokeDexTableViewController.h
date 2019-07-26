bcc6be6f8f996d486e03a315966c07afefc2108c
Michael R.F <mrflowers22@uwalumni.com>
2019-07-26 09:21:53 -0700

    Assigned the viewControllers to their applicable source code files.

---
+++ PokeDexObjc/PokeDexObjc/Storyboard/MRFPokeDexTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  MRFPokeDexTableViewController.h
+//  PokeDexObjc
+//
+//  Created by Michael Flowers on 7/26/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MRFPokeDexTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
