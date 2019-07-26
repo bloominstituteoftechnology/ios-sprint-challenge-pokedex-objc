20bc394eb54cae93d7800e6c406cf3ee7c4e6fc5
dllewisjolley <dlewisjolley@gmail.com>
2019-07-26 12:23:25 -0400

    model setup

---
+++ ObjcPokemon/ObjcPokemon/Model/DLJPokemon.h
@@ -0,0 +1,25 @@
+//
+//  DLJPokemon.h
+//  ObjcPokemon
+//
+//  Created by Diante Lewis-Jolley on 7/26/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DLJPokemon : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSURL *url;
+@property (nonatomic) int *pokemonID;
+@property (nonatomic, copy, nullable) NSArray<NSString *> *ablities;
+@property (nonatomic, copy, nullable) NSString *sprite;
+
+-(instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjcPokemon/ObjcPokemon/Model/DLJPokemon.m
@@ -0,0 +1,13 @@
+//
+//  DLJPokemon.m
+//  ObjcPokemon
+//
+//  Created by Diante Lewis-Jolley on 7/26/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+#import "DLJPokemon.h"
+
+@implementation DLJPokemon
+
+@end
