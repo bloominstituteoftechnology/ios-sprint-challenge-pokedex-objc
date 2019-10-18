d4b2e3ac0049193c75dc9c3bff5a1c64ac282b4a
luqmaan <luq.j.khan@gmail.com>
2019-10-18 11:20:45 -0500

    add fetch all pokemons method and succesfully displays on tableview

--- ObjcPokeDex/ObjcPokeDex/PokemonAPI.swift
+++ ObjcPokeDex/ObjcPokeDex/PokemonAPI.swift
@@ -15,7 +15,29 @@ class PokemonAPI: NSObject {
     let baseURL = URL(string: "https://pokeapi.co/api/v2")!
     
     @objc func fetchAllPokemon(completion: @escaping ([LSIPokemon]?, Error?) -> Void) {
-        
+        let requestURL = baseURL.appendingPathComponent("pokemon")
+        let request = URLRequest(url: requestURL)
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching all pokemons: \(error)")
+                completion(nil, error)
+                return
+            }
+            guard let data = data else {
+                NSLog("Error with fetch all pokemon data: \(String(describing: error))")
+                completion(nil, error)
+                return
+            }
+            do {
+                guard let jsonDict = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any], let results = jsonDict["results"] as? [[String:Any]]  else {throw NSError()}
+                let allPokemons = results.compactMap {LSIPokemon(dictionaryToGetNameAndURL: $0)}
+                completion(allPokemons, nil)
+            }catch {
+                NSLog("Error decoding all pokemons: \(error)")
+                completion(nil, error)
+                return
+            }
+        }.resume()
     }
     
     @objc func fillInDetails(for pokemon: LSIPokemon) {
---
+++ ObjcPokeDex/ObjcPokeDex/ViewControllers/PokemonsTableViewController.m
@@ -0,0 +1,51 @@
+//
+//  PokemonsTableViewController.m
+//  ObjcPokeDex
+//
+//  Created by Luqmaan Khan on 10/18/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+#import "PokemonsTableViewController.h"
+#import "LSIPokemon.h"
+#import "ObjcPokeDex-Swift.h"
+
+@interface PokemonsTableViewController ()
+
+@property (nonatomic, strong) NSArray<LSIPokemon *> *pokemons;
+
+@end
+
+@implementation PokemonsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    PokemonAPI *pa = [PokemonAPI sharedController];
+    [pa fetchAllPokemonWithCompletion:^(NSArray<LSIPokemon *> *allPokemons, NSError *error) {
+        self.pokemons = allPokemons;
+        dispatch_async(dispatch_get_main_queue(), ^{
+                   [self.tableView reloadData];
+               });
+    }];
+}
+
+#pragma mark - Table view data source
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.pokemons.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PokeCell" forIndexPath:indexPath];
+    LSIPokemon *pokemon = self.pokemons[indexPath.row];
+    cell.textLabel.text = pokemon.name;
+    return cell;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+}
+
+
+@end
