27b9a5971eec97d7cae5ea460b25896652dfbdf3
bradleyyin <bradleyyin@gmail.com>
2019-10-18 09:21:07 -0700

    add view controllers

---
+++ pokedex-objc/pokedex-objc/PokemonDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonDetailViewController.swift
+//  pokedex-objc
+//
+//  Created by Bradley Yin on 10/18/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
