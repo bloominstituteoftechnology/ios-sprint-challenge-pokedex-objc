c0d7225a7ab510e0ab913e39c8788908a9aff8de
bradleyyin <bradleyyin@gmail.com>
2019-10-18 08:24:03 -0700

    create project

---
+++ pokedex-objc/pokedex-objc/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  pokedex-objc
+//
+//  Created by Bradley Yin on 10/18/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
