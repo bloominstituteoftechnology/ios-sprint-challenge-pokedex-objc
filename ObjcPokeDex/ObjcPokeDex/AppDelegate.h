652e7debed84d6225a35dc92231bd22a6e9ca4a9
luqmaan <luq.j.khan@gmail.com>
2019-10-18 10:12:17 -0500

    add storyboards and elements

---
+++ ObjcPokeDex/ObjcPokeDex/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ObjcPokeDex
+//
+//  Created by Luqmaan Khan on 10/18/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ObjcPokeDex/ObjcPokeDex/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ObjcPokeDex
+//
+//  Created by Luqmaan Khan on 10/18/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
