fdd8e8a1e101fde35a93b7c7305b4a5ec068b22a
luqmaan <luq.j.khan@gmail.com>
2019-10-18 10:14:42 -0500

    add properties to LSIPokemon header

---
+++ ObjcPokeDex/ObjcPokeDex/LSIPokemon.h
@@ -0,0 +1,24 @@
+//
+//  LSIPokemon.h
+//  ObjcPokeDex
+//
+//  Created by Luqmaan Khan on 10/18/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIPokemon : NSObject
+
+@property (nullable) NSNumber *ID;
+@property (nonnull) NSString *name;
+@property (nonnull) NSString *pokeInfoURL;
+@property (nullable) NSMutableArray<NSString *> *abilites;
+@property (nullable) NSString *spriteURLString;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjcPokeDex/ObjcPokeDex/LSIPokemon.m
@@ -0,0 +1,13 @@
+//
+//  LSIPokemon.m
+//  ObjcPokeDex
+//
+//  Created by Luqmaan Khan on 10/18/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+#import "LSIPokemon.h"
+
+@implementation LSIPokemon
+
+@end
