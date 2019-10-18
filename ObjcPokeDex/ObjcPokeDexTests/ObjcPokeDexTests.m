bf93518e9c767cc6c5d680c05f9d03c68f7c5a45
luqmaan <luq.j.khan@gmail.com>
2019-10-18 10:27:03 -0500

    add test stubs

--- ObjcPokeDex/ObjcPokeDexTests/ObjcPokeDexTests.m
+++ ObjcPokeDex/ObjcPokeDexTests/ObjcPokeDexTests.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "LSIPokemon.h"
+#import "LSIFileHelper.h"
 
 @interface ObjcPokeDexTests : XCTestCase
 
@@ -14,24 +16,13 @@
 
 @implementation ObjcPokeDexTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)testParseJSONForTableView {
+    
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)testParseJSONToFillInPokemonDetails {
+    
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end
