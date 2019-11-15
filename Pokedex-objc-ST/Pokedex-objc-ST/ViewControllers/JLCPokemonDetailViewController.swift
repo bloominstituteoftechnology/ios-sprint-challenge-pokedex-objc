688507bb66e4d257b1081ebd9ea44627ee69a80c
Jake <jlconnerly@gmail.com>
2019-11-15 10:22:59 -0600

    Completed Storyboard setup and created respective files

---
+++ Pokedex-objc-ST/Pokedex-objc-ST/ViewControllers/JLCPokemonDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  JLCViewController.swift
+//  Pokedex-objc-ST
+//
+//  Created by Jake Connerly on 11/15/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class JLCPokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
--- Pokedex-objc-ST/Pokedex-objc-ST/ViewControllers/JLCTableViewController.h
+++ Pokedex-objc-ST/Pokedex-objc-ST/ViewControllers/JLCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  JLCTableViewController.h
 //  Pokedex-objc-ST
 //
 //  Created by Jake Connerly on 11/15/19.
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
 
+@interface JLCTableViewController : UITableViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
