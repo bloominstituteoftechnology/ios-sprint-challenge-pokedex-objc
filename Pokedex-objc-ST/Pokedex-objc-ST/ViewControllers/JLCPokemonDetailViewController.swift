3a4c6200b59b100dd5f10aa2c3628eec6011d6ca
Jake <jlconnerly@gmail.com>
2019-11-15 11:49:47 -0600

    Detailview now populates with pokemon name

--- Pokedex-objc-ST/Pokedex-objc-ST/ViewControllers/JLCPokemonDetailViewController.swift
+++ Pokedex-objc-ST/Pokedex-objc-ST/ViewControllers/JLCPokemonDetailViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class JLCPokemonDetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var abilitiesTextView: UITextView!
+    
+    
+    @objc var pokeController: PokeController?
+    @objc var pokeName: NSString?
+    var pokemon: JLCPokemon? {
+        didSet {
+            updateViews()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        fetchPokemon()
+    }
+    
+    private func fetchPokemon() {
+        guard let pokeController = pokeController,
+            let pokeName = pokeName as String? else { return }
+        pokeController.fetchSinglePokemon(with: pokeName) { (pokemon, error) in
+            if let error = error {
+                NSLog("Error when fetching pokemon \(pokeName) in detailview with error:\(error)")
+            }
+            DispatchQueue.main.async {
+                self.pokemon = pokemon
+            }
+        }
+    }
+    
+    private func updateViews() {
+        guard let pokemon = pokemon else { return }
+        nameLabel.text = "Name: \(pokemon.name)"
     }
 }
