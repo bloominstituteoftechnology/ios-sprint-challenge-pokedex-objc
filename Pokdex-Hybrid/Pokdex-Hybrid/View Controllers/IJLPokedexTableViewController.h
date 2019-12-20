9e6038d4ccb19c1630317f27c9346c43414c8499
Isvvc <lyons@tuta.io>
2019-12-20 09:19:52 -0700

    Create view controller subclasses

---
+++ Pokdex-Hybrid/Pokdex-Hybrid/View Controllers/IJLPokedexTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  IJLPokedexTableViewController.h
+//  Pokdex-Hybrid
+//
+//  Created by Isaac Lyons on 12/20/19.
+//  Copyright © 2019 Isaac Lyons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IJLPokedexTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokdex-Hybrid/Pokdex-Hybrid/View Controllers/PokemonDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonDetailViewController.swift
+//  Pokdex-Hybrid
+//
+//  Created by Isaac Lyons on 12/20/19.
+//  Copyright © 2019 Isaac Lyons. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
