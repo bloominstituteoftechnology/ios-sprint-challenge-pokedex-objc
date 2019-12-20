8b0798cc23e3884f18a10e5e84f0d6a2e1e821e8
Gi Pyo Kim <gipyok@gmail.com>
2019-12-20 08:08:10 -0800

    Initial commit

---
+++ Pokedex-Objc/Pokedex-Objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pokedex-Objc
+//
+//  Created by Gi Pyo Kim on 12/20/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pokedex-Objc/Pokedex-Objc/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Pokedex-Objc
+//
+//  Created by Gi Pyo Kim on 12/20/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
