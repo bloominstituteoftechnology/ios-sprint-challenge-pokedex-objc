376c97e7f75aa9f71baf077e561f3e9d9a7935da
Isvvc <lyons@tuta.io>
2019-12-20 09:13:05 -0700

    Create blank Xcode project

---
+++ Pokdex-Hybrid/Pokdex-Hybrid/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Pokdex-Hybrid
+//
+//  Created by Isaac Lyons on 12/20/19.
+//  Copyright © 2019 Isaac Lyons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
