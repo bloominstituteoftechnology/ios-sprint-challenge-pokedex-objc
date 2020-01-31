f9076a92cc5b7aa9bd8cf7c2c509cc6cb10b5df9
Percy Ngan <percival.ngan@gmail.com>
2020-01-31 13:27:06 -0600

    Added model file, PokeAPI and header file

---
+++ Pokedex-ObjC/Pokedex-ObjC/Models/PNCPokemon.h
@@ -0,0 +1,17 @@
+//
+//  PNCPokemon.h
+//  Pokedex-ObjC
+//
+//  Created by Lambda_School_Loaner_263 on 1/31/20.
+//  Copyright © 2020 Lambda_School_Loaner_263. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PNCPokemon : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex-ObjC/Pokedex-ObjC/Models/PNCPokemon.m
@@ -0,0 +1,13 @@
+//
+//  PNCPokemon.m
+//  Pokedex-ObjC
+//
+//  Created by Lambda_School_Loaner_263 on 1/31/20.
+//  Copyright © 2020 Lambda_School_Loaner_263. All rights reserved.
+//
+
+#import "PNCPokemon.h"
+
+@implementation PNCPokemon
+
+@end
