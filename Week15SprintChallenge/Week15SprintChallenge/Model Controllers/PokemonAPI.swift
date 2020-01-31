fd1a7349f7ad5fc5f6aab8143d1f5a51647ff258
Dan M <djmthedev@gmail.com>
2020-01-31 08:49:28 -0800

    added turn dictionary into [Pokemon]

--- Week15SprintChallenge/Week15SprintChallenge/Model Controllers/PokemonAPI.swift
+++ Week15SprintChallenge/Week15SprintChallenge/Model Controllers/PokemonAPI.swift
@@ -14,20 +14,33 @@ class PokemonAPI: NSObject {
     
     private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
     
-    
-    
-    
     @objc func fetchAllPokemon(completion: @escaping ([DMOPokemon]?, Error?) -> Void) {
 
         let url = urlForFetch(searchTerm: "")
         
         fetch(from: url) { (dictionary, error) in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
             
+            guard let dictionary = dictionary else {
+                completion(nil, NSError())
+                return
+            }
+            guard let pokemonDictionaries = dictionary["results"] as? [[String: String]] else {
+                print("Conversion to [[String: String]] failed.", dictionary["results"] ?? "no results")
+                return
+            }
+            var pokemen: [DMOPokemon] = []
+            for dictionary in pokemonDictionaries {
+                if let pokemon = DMOPokemon(dictionary: dictionary) {
+                    pokemen.append(pokemon)
+                }
+            }
+            print(pokemen.count)
+            completion(pokemen, nil)
         }
-        
-        
-        
-        
     }
     
     private func fetch(from url: URL, using session: URLSession = URLSession.shared, completion: @escaping ([String: Any]?, Error?) -> Void) {
@@ -52,7 +65,6 @@ class PokemonAPI: NSObject {
                     completion(nil, NSError())
                     return
                 }
-                print(dictionary)
                 completion(dictionary, nil)
                 return
                 
