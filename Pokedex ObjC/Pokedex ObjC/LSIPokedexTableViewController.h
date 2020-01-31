eec6c3ebad01a44c38342349dac11c8d5757f9f8
Wilma <kima_92@fastmail.com>
2020-01-31 11:22:35 -0500

    Setup UI

---
+++ Pokedex ObjC/Pokedex ObjC/LSIPokedexTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  LSIPokedexTableViewController.h
+//  Pokedex ObjC
+//
+//  Created by macbook on 1/31/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIPokedexTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Pokedex ObjC/Pokedex ObjC/LSIPokemonDetailViewController.h
+++ Pokedex ObjC/Pokedex ObjC/LSIPokemonDetailViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LSIPokemonDetailViewController.h
 //  Pokedex ObjC
 //
 //  Created by macbook on 1/31/20.
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
 
+@interface LSIPokemonDetailViewController : UIViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
