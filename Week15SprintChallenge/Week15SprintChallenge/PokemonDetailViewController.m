4e51029c903f23201090002c37259485ffde637d
Dan M <djmthedev@gmail.com>
2020-01-31 08:14:00 -0800

    initial added UI changed name of view controller to PokemonDetailViewController

---
+++ Week15SprintChallenge/Week15SprintChallenge/PokemonDetailViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Week15SprintChallenge
+//
+//  Created by morse on 1/31/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+#import "PokemonDetailViewController.h"
+
+@interface PokemonDetailViewController ()
+
+@end
+
+@implementation PokemonDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ Week15SprintChallenge/Week15SprintChallenge/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Week15SprintChallenge
+//
+//  Created by morse on 1/31/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
