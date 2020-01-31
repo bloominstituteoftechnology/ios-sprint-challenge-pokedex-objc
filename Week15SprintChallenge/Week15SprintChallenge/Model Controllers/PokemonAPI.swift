891d00293a2ce73869e00904872dddb5e150150a
Dan M <djmthedev@gmail.com>
2020-01-31 08:45:17 -0800

    added url for fetching all pokemen

--- Week15SprintChallenge/Week15SprintChallenge/Model Controllers/PokemonAPI.swift
+++ Week15SprintChallenge/Week15SprintChallenge/Model Controllers/PokemonAPI.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class PokemonAPI: NSObject {
+    
+    @objc(sharedController) static let shared = PokemonAPI()
+    
+    private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    
+    
+    
+    
+    @objc func fetchAllPokemon(completion: @escaping ([DMOPokemon]?, Error?) -> Void) {
+
+        let url = urlForFetch(searchTerm: "")
+        
+        print(url)
+        
+        
+        
+        
+    }
+    
+    private func urlForFetch(searchTerm: String) -> URL {
+        var url = baseURL
+        if !searchTerm.isEmpty {
+            url.appendPathComponent(searchTerm)
+        } else {
+            var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+            let limitItem = URLQueryItem(name: "limit", value: "1000")
+            components.queryItems = [limitItem]
+            url = components.url!
+        }
+        return url
+    }
+    
+}
--- Week15SprintChallenge/Week15SprintChallenge/View Controllers/DMOPokemenTableViewController.m
+++ Week15SprintChallenge/Week15SprintChallenge/View Controllers/DMOPokemenTableViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "DMOPokemenTableViewController.h"
+#import "Week15SprintChallenge-Swift.h"
+#import "DMOPokemon.h"
 
 @interface DMOPokemenTableViewController ()
 
@@ -17,11 +19,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [PokemonAPI.sharedController fetchAllPokemonWithCompletion:^(NSArray<DMOPokemon *> *pokemonNames, NSError *error) {
+        
+        
+    }];
 }
 
 #pragma mark - Table view data source
