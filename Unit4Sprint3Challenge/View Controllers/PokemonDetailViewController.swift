f24adba02dcf5c73427c45f7d2dc2b1cd5db095f
Jon Bash <jonbash@protonmail.com>
2020-01-31 08:38:39 -0800

    set up UI

---
+++ Unit4Sprint3Challenge/View Controllers/PokemonDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonDetailViewController.swift
+//  Unit4Sprint3Challenge
+//
+//  Created by Jon Bash on 2020-01-31.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
--- Unit4Sprint3Challenge/View Controllers/PokemonTableViewController.h
+++ Unit4Sprint3Challenge/View Controllers/PokemonTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PokemonTableViewController.h
 //  Unit4Sprint3Challenge
 //
 //  Created by Jon Bash on 2020-01-31.
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
 
+@interface PokemonTableViewController : UITableViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
