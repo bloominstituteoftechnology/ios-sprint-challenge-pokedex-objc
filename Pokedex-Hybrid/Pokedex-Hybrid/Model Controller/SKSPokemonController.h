8e261087b91362d6a621a5cf007cf61c6f0b71ea
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-01-31 08:57:09 -0800

    Added pokemoncontroller to take care of the fetched pokemon.

---
+++ Pokedex-Hybrid/Pokedex-Hybrid/Model Controller/SKSPokemonController.h
@@ -0,0 +1,17 @@
+//
+//  SKSPokemonController.h
+//  Pokedex-Hybrid
+//
+//  Created by Lambda_School_Loaner_204 on 1/31/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SKSPokemonController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex-Hybrid/Pokedex-Hybrid/Model Controller/SKSPokemonController.m
@@ -0,0 +1,13 @@
+//
+//  SKSPokemonController.m
+//  Pokedex-Hybrid
+//
+//  Created by Lambda_School_Loaner_204 on 1/31/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "SKSPokemonController.h"
+
+@implementation SKSPokemonController
+
+@end
