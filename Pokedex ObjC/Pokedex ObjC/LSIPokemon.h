d11395ad6449327956f98fb5bee081bea339dfdd
Wilma <kima_92@fastmail.com>
2020-01-31 11:40:32 -0500

    Pokemon Model

---
+++ Pokedex ObjC/Pokedex ObjC/LSIPokemon.h
@@ -0,0 +1,28 @@
+//
+//  LSIPokemon.h
+//  Pokedex ObjC
+//
+//  Created by macbook on 1/31/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIPokemon : NSObject
+
+@property (nonatomic, copy, nonnull) NSString *name;
+@property (nonatomic, copy, nonnull) NSURL *url;
+
+@property (nonatomic) int identifier;
+@property (nonatomic, nonnull) NSURL *imageURL;
+@property (nonatomic, copy, nonnull) NSMutableArray *abilities;
+
+
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
