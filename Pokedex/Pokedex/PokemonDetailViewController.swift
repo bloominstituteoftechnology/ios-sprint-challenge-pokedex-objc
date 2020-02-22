5efff5be7be26656ba392e9ae72e9948d583f344
Robert Keffury <bobby.keffury@me.com>
2020-02-22 13:36:46 -0800

    Added PokemonTableViewController, organized it, and wired it up.

---
+++ Pokedex/Pokedex/PokemonDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PokemonDetailViewController.swift
+//  Pokedex
+//
+//  Created by Bobby Keffury on 2/22/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pokedex/Pokedex/PokemonTableViewController.m
@@ -0,0 +1,43 @@
+//
+//  PokemonTableViewController.m
+//  Pokedex
+//
+//  Created by Bobby Keffury on 2/22/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+#import "PokemonTableViewController.h"
+
+@interface PokemonTableViewController ()
+
+@end
+
+@implementation PokemonTableViewController
+
+
+//MARK: - Views
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+//MARK: - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PokemonCell" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+
+//MARK: - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+}
+
+
+@end
