c8766a5bf76b06254382b38acc44a53475acd462
Robert Keffury <bobby.keffury@me.com>
2020-02-22 13:46:49 -0800

    Added PokemonController for networking and created skeleton.

---
+++ Pokedex/Pokedex/PokemonController.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonController.swift
+//  Pokedex
+//
+//  Created by Bobby Keffury on 2/22/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum HTTPMethod: String {
+    case get = "GET"
+}
+
+enum NetworkError: Error {
+    case badData
+    case otherError
+    case noDecode
+}
+
+class PokemonController {
+    
+}
