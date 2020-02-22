a9dbc624abf7101fd1b257ca3aca174a2f4715a8
Fabiola <saga.fabiola@gmail.com>
2020-02-22 09:26:29 -0600

    Implemented DetailVC

--- Pokedex-ObjC/Pokedex-ObjC/PokemonDetailViewController.m
+++ Pokedex-ObjC/Pokedex-ObjC/PokemonDetailViewController.m
@@ -25,8 +25,49 @@ void *KVOContext = &KVOContext;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [PokemonController.sharedController fetchPokemonDetailsWithPokemon:self.pokemon];
 }
+
+- (void)updateViews
+{
+    if (!self.isViewLoaded || !self.pokemon) { return; }
+    self.pokemonName.text = self.pokemon.name;
+    self.pokemonID.text = [NSString stringWithFormat:@"%d", self.pokemon.id];
+    self.pokemonAbilities.text = [self.pokemon.abilities componentsJoinedByString:@", "];
+    
+    NSData *imageData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:self.pokemon.sprites]];
+    self.pokemonImage.image = [[UIImage alloc] initWithData:imageData];
+}
+
+- (void)setPokemon:(Pokemon *)pokemon
+{
+    if (_pokemon != pokemon) {
+        [_pokemon removeObserver:self forKeyPath:@"name" context:KVOContext];
+        [_pokemon removeObserver:self forKeyPath:@"id" context:KVOContext];
+        [_pokemon removeObserver:self forKeyPath:@"sprites" context:KVOContext];
+        [_pokemon removeObserver:self forKeyPath:@"abilities" context:KVOContext];
+        
+        _pokemon = pokemon;
+        
+        [_pokemon addObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionInitial context:KVOContext];
+        [_pokemon addObserver:self forKeyPath:@"id" options:NSKeyValueObservingOptionInitial context:KVOContext];
+        [_pokemon addObserver:self forKeyPath:@"sprites" options:NSKeyValueObservingOptionInitial context:KVOContext];
+        [_pokemon addObserver:self forKeyPath:@"abilities" options:NSKeyValueObservingOptionInitial context:KVOContext];
+    }
+    
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context
+{
+    if (context == KVOContext) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self updateViews];
+        });
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
 
 /*
 #pragma mark - Navigation
