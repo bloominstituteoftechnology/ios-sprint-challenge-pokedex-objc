81fa80c85c7a38b3d38cea8020214cf34c7c4154
John Kouris <jdkouris@gmail.com>
2020-02-22 09:35:08 -0800

    Added the VC files and connected them to storyboard

---
+++ PokedexHybrid/PokedexHybrid/PokemonDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonDetailViewController.swift
+//  PokedexHybrid
+//
+//  Created by John Kouris on 2/22/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
