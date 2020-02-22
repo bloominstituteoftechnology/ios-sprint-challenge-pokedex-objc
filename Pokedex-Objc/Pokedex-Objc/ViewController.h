47f166b260a899dd90b5b8ca925ddb0ed815f112
Joel Groomer <joelgroomer@gmail.com>
2020-02-22 09:37:39 -0800

    Created and organized project.

---
+++ Pokedex-Objc/Pokedex-Objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pokedex-Objc
+//
+//  Created by Joel Groomer on 2/22/20.
+//  Copyright © 2020 Julltron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pokedex-Objc/Pokedex-Objc/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Pokedex-Objc
+//
+//  Created by Joel Groomer on 2/22/20.
+//  Copyright © 2020 Julltron. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
