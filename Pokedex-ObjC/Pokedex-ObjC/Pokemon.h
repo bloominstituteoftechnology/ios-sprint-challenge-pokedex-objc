aa26fd8156e95f4c40fc4c0beccb72e70d741a5b
Fabiola <saga.fabiola@gmail.com>
2020-02-21 22:35:47 -0600

    Added Pokemon model, turned off ARC

---
+++ Pokedex-ObjC/Pokedex-ObjC/Pokemon.h
@@ -0,0 +1,32 @@
+//
+//  Pokemon.h
+//  Pokedex-ObjC
+//
+//  Created by Fabiola S on 2/21/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Pokemon : NSObject
+
+@property (nonatomic, copy, nullable) NSString *name;
+@property (nonatomic) int id;
+@property (nonatomic, copy, nullable) NSArray *abilities;
+@property (nonatomic, copy, nullable) NSString *url;
+@property (nonatomic, copy, nullable) NSString *sprites;
+
+- (nonnull instancetype)initWithName:(nullable NSString *)name
+                                 url:(nullable NSString *)url
+                                  id:(int)id
+                             sprites:(nullable NSString *)sprites
+                           abilities:(nullable NSArray *)abilities;
+
+- (nullable instancetype)initWithDictionary:(nonnull NSDictionary *)dictionary;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
