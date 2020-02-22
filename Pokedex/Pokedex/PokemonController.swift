9ad22944e77d2f19afca10b7fb9113aca1a42fd4
Robert Keffury <bobby.keffury@me.com>
2020-02-22 14:00:27 -0800

    Created FetchPokemon func. Needs some work.

--- Pokedex/Pokedex/PokemonController.swift
+++ Pokedex/Pokedex/PokemonController.swift
@@ -23,6 +23,40 @@ class PokemonController {
     
     //MARK: - Properties
     
+    var pokemonArray: [String] = []
+    
+    private let baseUrl = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    
     //MARK: - Methods
     
+    func fetchPokemon(completion: @escaping (Result<[String], NetworkError>) -> Void) {
+        
+        var request = URLRequest(url: baseUrl)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            
+            if let _ = error {
+                completion(.failure(.otherError))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let names = try decoder.decode([String], from: data)
+                completion(.success(names))
+                
+            } catch {
+                print("Error decoding pokemon names: \(error)")
+                completion(.failure(.noDecode))
+                return
+            }
+        }.resume()
+    }
 }
