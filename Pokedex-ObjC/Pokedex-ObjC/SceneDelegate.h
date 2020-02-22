92e8f7a2a276fef2b54292be999dff578942c77f
Fabiola <saga.fabiola@gmail.com>
2020-02-21 21:33:03 -0600

    Added Pokedex Project, and created folders to organize files

---
+++ Pokedex-ObjC/Pokedex-ObjC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Pokedex-ObjC
+//
+//  Created by Fabiola S on 2/21/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Pokedex-ObjC/Pokedex-ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pokedex-ObjC
+//
+//  Created by Fabiola S on 2/21/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
