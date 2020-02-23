1a0a86d73f10e5a3ed1a5dc1953430808cf48c7d
Vici Shaweddy <vshaweddy@gmail.com>
2020-02-23 00:57:12 -0500

    KVO setup

--- Pokedex/Pokedex/View Controllers/VVSPokemonViewController.m
+++ Pokedex/Pokedex/View Controllers/VVSPokemonViewController.m
@@ -9,6 +9,8 @@
 #import "VVSPokemonViewController.h"
 #import <UIKit/UIKit.h>
 
+void *KVOContext = &KVOContext;
+
 @interface VVSPokemonViewController ()
 
 @end
@@ -18,33 +20,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self addObserver:self
+           forKeyPath:@"pokemon"
+              options:kNilOptions
+              context:KVOContext];
     
     [self.pokemonController fetchPokemonWithName:self.pokemon.name completion:^(VVSPokemon * _Nullable pokemon, NSError * _Nullable error) {
         if (error) {
             NSLog(@"Error: %@", error);
             return;
         }
-
-        NSData *imageData = [[NSData alloc] initWithContentsOfURL:pokemon.spriteURL];
         
         dispatch_async(dispatch_get_main_queue(), ^{
-            self.imageView.image = [UIImage imageWithData:imageData];
-            self.nameLabel.text = [NSString stringWithFormat:@"Name: %@", [pokemon.name capitalizedString]];
-            self.idLabel.text = [NSString stringWithFormat:@"ID: %@", pokemon.identifier];
-            self.abilitiesLabel.text = [NSString stringWithFormat:@"Abilities: %@", [[pokemon abilities] componentsJoinedByString:@", "]];
+            self.pokemon = pokemon;
         });
-        
     }];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)dealloc
+{
+    [self removeObserver:self
+              forKeyPath:@"pokemon"
+                 context:KVOContext];
 }
-*/
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if (context == KVOContext) {
+        if ([keyPath isEqualToString:@"pokemon"]) {
+            NSData *imageData = [[NSData alloc] initWithContentsOfURL:self.pokemon.spriteURL];
+        
+            self.imageView.image = [UIImage imageWithData:imageData];
+            self.nameLabel.text = [NSString stringWithFormat:@"Name: %@", [self.pokemon.name capitalizedString]];
+            self.idLabel.text = [NSString stringWithFormat:@"ID: %@", self.pokemon.identifier];
+            self.abilitiesLabel.text = [NSString stringWithFormat:@"Abilities: %@", [[self.pokemon abilities] componentsJoinedByString:@", "]];
+        }
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
 
 @end
