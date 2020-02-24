850bd6e679b825536aa47ded1f2c19f9d01b1136
Fabiola <saga.fabiola@gmail.com>
2020-02-23 22:12:24 -0600

    Cleaned up UI

--- Pokedex-ObjC/Pokedex-ObjC/ShadowView.swift
+++ Pokedex-ObjC/Pokedex-ObjC/ShadowView.swift
@@ -14,8 +14,8 @@ class ShadowView: UIView {
     override func awakeFromNib() {
         layer.shadowPath = CGPath(rect: layer.bounds, transform:  nil)
         layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOffset = CGSize(width: 0.8, height: 0.8)
-        layer.shadowOpacity = 0.2
+        layer.shadowOffset = CGSize(width: 0.9, height: 0.10)
+        layer.shadowOpacity = 0.3
         layer.shadowRadius = 25
         
         layer.cornerRadius = 25
