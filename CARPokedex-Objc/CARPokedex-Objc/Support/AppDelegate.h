04daa6260b985858dc20421b8afaef66937d9919
Chad Rutherford <chad_rutherford@outlook.com>
2020-02-28 11:08:22 -0500

    Project Restructure

---
+++ CARPokedex-Objc/CARPokedex-Objc/Support/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CARPokedex-Objc
+//
+//  Created by Chad Rutherford on 2/28/20.
+//  Copyright © 2020 Chad Rutherford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
