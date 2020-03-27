d3fd5f543d0a050581e257b3fa7c11ae0174831e
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-27 14:37:55 -0700

    added capitalized names, plus image loader

--- PokedexSprint/PokedexSprint/Model Controller/PokemonAPI.swift
+++ PokedexSprint/PokedexSprint/Model Controller/PokemonAPI.swift
@@ -10,7 +10,6 @@ import Foundation
 
 @objc class PokemonAPI: NSObject {
 
-    // TODO: this is yelling at me
     @objc (sharedController) static let shared = PokemonAPI()
 
     @objc func fetchAllPokemon(completion: @escaping ([JLAPokemon]?, Error?) -> Void) {
@@ -31,48 +30,10 @@ import Foundation
                 print("no data")
                 return
             }
-            
-            /*
-             error = nil;
-             NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
-             
-             if (error) {
-                 NSLog(@"Error decoding JSON from data: %@", error);
-                 completion(nil);
-                 return;
-             }
-             
-             if(!dictionary || ![dictionary isKindOfClass:[NSDictionary class]]) {
-                 NSLog(@"Error: Expected top level dictionary in JSON result: %@", error);
-                 completion(nil);
-                 return;
-             }
-             
-             NSLog(@"Dictionary: %@", dictionary);
-             
-            */
-            
-            /*
-             {
-             "count": 964,
-             "next": "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20",
-             "previous": null,
-             "results": [
-               {
-                 "name": "bulbasaur",
-                 "url": "https://pokeapi.co/api/v2/pokemon/1/"
-               },
-               {
-                 "name": "ivysaur",
-                 "url": "https://pokeapi.co/api/v2/pokemon/2/"
-               },
-             */
                     
-            // NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
             do {
                 let dictionary = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let results = dictionary["results"] as! [[String: Any]] // array of array of dictionaries
-                //self.pokemonArray.append(decodedPokemon)
                 print("dictionary: \(dictionary)")
                 print("results: \(results)")
                 let pokemonResults = results.map {JLAPokemon(dictionary: $0)}
--- PokedexSprint/PokedexSprint/View Controller/JLADetailViewController.m
+++ PokedexSprint/PokedexSprint/View Controller/JLADetailViewController.m
@@ -36,8 +36,32 @@
     if (self.pokemon) {
     
         // TODO: add #self.pokemon.identifier -
-        NSString *title = [NSString stringWithFormat:@"%@", self.pokemon.name];
+        NSString *title = [NSString stringWithFormat:@"%@", self.pokemon.name.capitalizedString];
         self.title = title;
+        
+        NSURL *url = self.pokemon.sprite;
+        NSLog(@"url = %@", url);
+        [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                if (error) {
+                    NSLog(@"Error: %@", error);
+                    return;
+                }
+                // TODO: data is nil
+        //        NSData -> UIImage
+                UIImage *image = [[UIImage alloc] initWithData:data];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    NSLog(@"Image: %@", NSStringFromCGSize(image.size));
+                    self.imageView.image = image;
+                });
+            }] resume];
+    }
+}
+
+- (void)setPokemon:(JLAPokemon *)pokemon {
+   
+    if (pokemon != _pokemon) {
+        
+        _pokemon = pokemon;
     }
 }
 
