ae6c5bce73d8e77106d569d3ce56f40318809ee8
Aaron Cleveland <toscleveland@gmail.com>
2020-03-27 16:28:13 -0500

    fixed one mainstoryboard error

---
+++ Pokedex/Pokedex/Controllers/View Controllers/AMCPokemonDetailViewController.h
@@ -0,0 +1,21 @@
+//
+//  AMCPokemonDetailViewController.h
+//  Pokedex
+//
+//  Created by Aaron Cleveland on 3/27/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class AMCPokemon;
+
+@interface AMCPokemonDetailViewController : UIViewController
+
+@property (nonatomic) AMCPokemon *pokemon;
+
+@end
+
+NS_ASSUME_NONNULL_END
