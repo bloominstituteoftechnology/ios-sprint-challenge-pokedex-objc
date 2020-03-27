8414500b217ae42903394b687713824badbbfbd2
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-27 08:07:02 -0700

    added project

---
+++ PokedexSprint/PokedexSprint/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PokedexSprint
+//
+//  Created by Jorge Alvarez on 3/27/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
