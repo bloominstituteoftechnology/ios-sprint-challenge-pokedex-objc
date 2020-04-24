0e7c4e4eb829b04e203c8ebd978668c423483cd2
Nick <ptnguyen1901@gmail.com>
2020-04-24 12:13:42 -0400

    Fetch all pokemons and display on table view.

--- PokeObjC/PokeObjC/Networking/PokemonAPI.swift
+++ PokeObjC/PokeObjC/Networking/PokemonAPI.swift
@@ -9,10 +9,61 @@
 import UIKit
 
 class PokemonAPI: NSObject {
-   
+     
+    let allPokemonURL = URL(string: "https://pokeapi.co/api/v2/pokemon/?limit=1000")!
+    var pokemons : [NPTPokemon] = []
+    
     @objc(sharedController) static let shared = PokemonAPI()
     
-//    @objc func fetchAllPokemon(completion: @escaping ([LSIPokemon]?, Error?) -> Void)
-//    
-//    @objc func fillInDetails(for pokemon: LSIPokemon)
+    @objc func fetchAllPokemon(completion: @escaping ([NPTPokemon]?, Error?) -> Void) {
+        URLSession.shared.dataTask(with: allPokemonURL) { (data, response, error) in
+            if let response = response as? HTTPURLResponse, response.statusCode < 200 || response.statusCode > 299 {
+                completion(nil,errorWithMessage("bad response", LSIAPIError.rawValue))
+                return
+            }
+            if let err = error as NSError? {
+                completion(nil,err)
+                return
+            }
+            guard let data = data else {
+                completion(nil,errorWithMessage("No data", LSIPlaceError.rawValue))
+                return
+            }
+            var topLevelDictionary : [String:Any?]
+            do {
+                topLevelDictionary = try (JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] ?? [:])
+            } catch let error as NSError {
+                completion(nil,error)
+                return
+            }
+            guard let resultsArray = topLevelDictionary["results"] as? [[String:String]]else { return }
+            
+            for pokemonDict in resultsArray {
+                guard let pokemon = NPTPokemon(dictionary: pokemonDict) else { return }
+                self.pokemons.append(pokemon)
+                completion(self.pokemons,nil)
+            }
+            
+            
+            
+            
+        }.resume()
+    }
+    
+    
+    
+
+    @objc func fillInDetails(for pokemon: NPTPokemon) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- PokeObjC/PokeObjC/View Controller/MainTableViewController.m
+++ PokeObjC/PokeObjC/View Controller/MainTableViewController.m
@@ -7,20 +7,61 @@
 //
 
 #import "MainTableViewController.h"
+#import "NPTPokemon.h"
+#import "LSILog.h"
+#import "PokeObjC-Swift.h"
 
 @interface MainTableViewController ()
+
+@property (nonnull, nonatomic) NSArray<NPTPokemon *> *pokemons;
+
 
 @end
 
 @implementation MainTableViewController
 
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        _pokemons = @[];
+    }
+    return self;
+}
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    NSSortDescriptor *sortDescriptor;
+    sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];
+    NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
+    
+    [PokemonAPI.sharedController fetchAllPokemonWithCompletion:^(NSArray<NPTPokemon *> * _Nullable pokemons, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"%@",error);
+        }
+        
+        self.pokemons = [pokemons sortedArrayUsingDescriptors:sortDescriptors];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+    }];
  
 }
 
 #pragma mark - Table view data source
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.pokemons.count;
+}
 
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PokeCell" forIndexPath:indexPath];
+    NPTPokemon *pokemon = self.pokemons[indexPath.row];
+    cell.textLabel.text = [pokemon.name capitalizedString];
+    return cell;
+}
 
 @end
