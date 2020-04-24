52c8e1b25fbed098c3887ed436318769e8aa86a8
Enrique Gongora <EGTech24@gmail.com>
2020-04-23 22:19:22 -0700

    Initial Commit

---
+++ Pokedex/Pokedex/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pokedex
+//
+//  Created by Enrique Gongora on 4/23/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Pokedex/Pokedex/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Pokedex
+//
+//  Created by Enrique Gongora on 4/23/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
