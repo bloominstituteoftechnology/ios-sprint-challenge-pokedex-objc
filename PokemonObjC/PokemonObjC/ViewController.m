02ede9cad57349536f0e8cb714aa80f0ee0d26da
Chris <chris.gonzales.82@gmail.com>
2020-04-24 08:54:20 -0700

    created project.  Completed ReadMe.

---
+++ PokemonObjC/PokemonObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PokemonObjC
+//
+//  Created by Chris Gonzales on 4/24/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
