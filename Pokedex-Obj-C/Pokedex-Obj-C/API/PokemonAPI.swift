9cbbff3c31104a712fa68818c5ee868a81625aab
scottharris86 <scottharrisdev@gmail.com>
2020-04-24 12:05:24 -0400

    update bridging header and update fetch all pokemon completion model

--- Pokedex-Obj-C/Pokedex-Obj-C/API/PokemonAPI.swift
+++ Pokedex-Obj-C/Pokedex-Obj-C/API/PokemonAPI.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+class PokemonAPI: NSObject {
+    
+    @objc(sharedController) static let shared: PokemonAPI = PokemonAPI()
+    
+    @objc func fetchAllPokemon(completion: @escaping ([SAHPokemon]?, Error?) -> Void) {
+        
+        
+    }
+    
+//    @objc func fillInDetails(for pokemon: LSIPokemon)
+}
