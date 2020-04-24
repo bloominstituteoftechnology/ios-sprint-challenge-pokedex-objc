4dc30037463b73bccce702f9e598f5ae2af7fcd3
scottharris86 <scottharrisdev@gmail.com>
2020-04-24 11:12:47 -0400

    initial project setup

---
+++ Pokedex-Obj-C/Pokedex-Obj-C/Application/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Pokedex-Obj-C
+//
+//  Created by scott harris on 4/24/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Pokedex-Obj-C/Pokedex-Obj-C/View Controllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pokedex-Obj-C
+//
+//  Created by scott harris on 4/24/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
