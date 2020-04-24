efbc31a2859ef0b5e14ac21f62db3cc792c51c83
Enrique Gongora <EGTech24@gmail.com>
2020-04-24 07:57:03 -0700

    Pokemon Detail screen completed.

---
+++ Pokedex/Pokedex/View Controllers/EGCPokedexTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  EGCPokedexTableViewController.h
+//  Pokedex
+//
+//  Created by Enrique Gongora on 4/24/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EGCPokedexTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/Pokedex/View Controllers/EGCPokedexTableViewController.m
@@ -0,0 +1,30 @@
+//
+//  EGCPokedexTableViewController.m
+//  Pokedex
+//
+//  Created by Enrique Gongora on 4/24/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+#import "EGCPokedexTableViewController.h"
+
+@interface EGCPokedexTableViewController ()
+
+@end
+
+@implementation EGCPokedexTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+@end
