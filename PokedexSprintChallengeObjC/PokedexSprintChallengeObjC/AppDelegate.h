0fd10c992a2aad43c43f2acfa5c148f6b45f0595
dillonp23 <dillonp.lambda@gmail.com>
2020-05-03 19:12:19 -0700

    Answered questions in Manual-Reference-Counting.md. Added Xcode starter project.

---
+++ PokedexSprintChallengeObjC/PokedexSprintChallengeObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PokedexSprintChallengeObjC
+//
+//  Created by Dillon P on 5/3/20.
+//  Copyright © 2020 Dillon's Lambda Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PokedexSprintChallengeObjC/PokedexSprintChallengeObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PokedexSprintChallengeObjC
+//
+//  Created by Dillon P on 5/3/20.
+//  Copyright © 2020 Dillon's Lambda Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
