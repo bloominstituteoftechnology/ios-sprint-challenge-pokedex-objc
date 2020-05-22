82b360f2a367987f9b2a57e98786b69e57b81daf
knopptimism <56057392+knopptimism@users.noreply.github.com>
2020-05-22 11:30:39 -0700

    Pokemon Header made

--- NotDigimonObjectiveC/NotDigimonObjectiveC/ModelAndController/MSKPokemon.h
+++ NotDigimonObjectiveC/NotDigimonObjectiveC/ModelAndController/MSKPokemon.h
@@ -7,11 +7,29 @@
 //
 
 #import <Foundation/Foundation.h>
-
+@class MSKSprites
+@class MSKAbility
+@class MSKMove
+@class MSKSpcies
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MSKPokemon : NSObject
-
+@interface MSKPokemon: NSObject
+@property (nonatomic, copy)  NSString *name;
+@property (nonatomic, assign) NSInteger *identifier;
+@property (nonatomic, assign) MSKSprites *sprites;
+@property (nonatomic, copy) NSArray<MSKAbility *> *abilities;
 @end
+
+@interface MSKSprites: NSObject
+@@property (nonatomic, copy) NSString *frontShiny;
+@end
+
+@interface MSKAbility: NSObject
+@@property (nonatomic, strong) MSKSpecies *ability;
+@end
+
+@interface MSKSpecies NSObject
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *url;
 
 NS_ASSUME_NONNULL_END
