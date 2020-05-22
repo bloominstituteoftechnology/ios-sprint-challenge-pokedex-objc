6966905090295819266c3aac8d2e0641ba576d95
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-22 12:10:41 -0400

    Created detail view controller

---
+++ Pokedex ObjC/Pokedex ObjC/View Controllers/WAHPokemonDetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  WAHPokemonDetailsViewController.h
+//  Pokedex ObjC
+//
+//  Created by Wyatt Harrell on 5/22/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WAHPokemonDetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex ObjC/Pokedex ObjC/View Controllers/WAHPokemonDetailsViewController.m
@@ -0,0 +1,42 @@
+//
+//  WAHPokemonDetailsViewController.m
+//  Pokedex ObjC
+//
+//  Created by Wyatt Harrell on 5/22/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+#import "WAHPokemonDetailsViewController.h"
+
+@interface WAHPokemonDetailsViewController ()
+
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *idLabel;
+@property (strong, nonatomic) IBOutlet UILabel *abilitiesLabel;
+
+
+
+
+@end
+
+@implementation WAHPokemonDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
