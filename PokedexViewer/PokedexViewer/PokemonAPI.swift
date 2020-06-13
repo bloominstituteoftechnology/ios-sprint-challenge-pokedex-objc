52683f306b89415044c544a049f8e9e933847838
Jessie <jessie.griffin@icloud.com>
2020-06-12 21:54:12 -0400

    Adds PokemonAPI

--- PokedexViewer/PokedexViewer/PokemonAPI.swift
+++ PokedexViewer/PokedexViewer/PokemonAPI.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+class PokemonAPI: NSObject {
+
+//    @objc(sharedController) static let shared: PokemonAPI
+//
+//    @objc func fetchAllPokemon(completion: @escaping ([LSIPokemon]?, Error?) -> Void)
+//
+//    @objc func fillInDetails(for pokemon: LSIPokemon)
+}
