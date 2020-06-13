f6dbf06f081eea67d1ca3d40e58d2f9841c400ed
Sal <salmanamer@gmail.com>
2020-06-12 18:27:33 -0700

    setup Project

---
+++ Pokedex/Pokedex/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pokedex
+//
+//  Created by Sal B Amer on 6/12/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Pokedex/Pokedex/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Pokedex
+//
+//  Created by Sal B Amer on 6/12/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
