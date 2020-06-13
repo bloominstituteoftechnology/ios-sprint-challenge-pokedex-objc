75d112c69a2a33170cc8265acf300b9df30d6230
Sal <salmanamer@gmail.com>
2020-06-12 20:56:44 -0700

    setup PokeDetailsViewController.swift

---
+++ Pokedex/Pokedex/View Controllers/PokeDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokeDetailsViewController.swift
+//  Pokedex
+//
+//  Created by Sal B Amer on 6/12/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+import UIKit
+
+class PokeDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
