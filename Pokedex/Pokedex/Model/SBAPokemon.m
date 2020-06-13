7b6bba7c6ec913b5d1f8d7f67b4c5ee209d6896c
Sal <salmanamer@gmail.com>
2020-06-12 18:49:18 -0700

    setup pokemon model headers and properties

---
+++ Pokedex/Pokedex/Model/SBAPokemon.m
@@ -0,0 +1,15 @@
+//
+//  SBAPokemon.m
+//  Pokedex
+//
+//  Created by Sal B Amer on 6/12/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+
+#import "SBAPokemon.h"
+
+@implementation SBAPokemon
+
+ 
+@end
