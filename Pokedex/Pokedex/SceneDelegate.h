3bae0384516ad2129805630bc8727514ea0aec2d
Enayatullah Naseri <enayatullahna@gmail.com>
2020-06-12 18:56:06 -0700

    Started Project, updated storyboard

---
+++ Pokedex/Pokedex/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Pokedex
+//
+//  Created by Enayatullah Naseri on 6/12/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
