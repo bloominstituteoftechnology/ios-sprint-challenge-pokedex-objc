df69e0bf89e6dd4e4486f28a1c0ce414d8ac9147
Alex Thompson <guitarman226@gmail.com>
2020-06-13 12:03:30 -0500

    Setup Model

---
+++ Pokedex/Pokedex/TSAPokemon.h
@@ -0,0 +1,25 @@
+//
+//  TSAPokemon.h
+//  Pokedex
+//
+//  Created by Alex Thompson on 6/13/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TSAPokemon : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *url;
+@property (nonatomic) int identifier;
+@property (nonatomic, copy) NSString *image;
+@property (nonatomic, copy) NSArray *abilites;
+
+- (instancetype)initWithName:(NSString *)name url:(NSString *)url;
+
+@end
+
+NS_ASSUME_NONNULL_END
