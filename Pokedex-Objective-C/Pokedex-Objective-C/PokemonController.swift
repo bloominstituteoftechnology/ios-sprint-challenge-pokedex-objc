a2a2c671f08cb10341d812e60f0a2c2df1872c7b
Alex Shillingford <alex.shillingford@icloud.com>
2020-06-14 20:57:07 -0600

    Create Model Controller File

---
+++ Pokedex-Objective-C/Pokedex-Objective-C/PokemonController.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonController.swift
+//  Pokedex-Objective-C
+//
+//  Created by Alex Shillingford on 6/14/20.
+//  Copyright © 2020 shillwil. All rights reserved.
+//
+
+import Foundation
