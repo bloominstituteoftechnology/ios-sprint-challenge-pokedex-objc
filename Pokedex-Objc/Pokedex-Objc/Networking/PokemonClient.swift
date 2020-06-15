82bddaeaa037bc5abf781b373988d01645d592f1
David Wright <davidtwright89@gmail.com>
2020-06-14 23:58:36 -0400

    Added a Swift PokemonClient class and a bridging header

---
+++ Pokedex-Objc/Pokedex-Objc/Networking/PokemonClient.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonClient.swift
+//  Pokedex-Objc
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import Foundation
+
+class PokemonClient {
+    
+}
