134422fd691a26d178e1465b33a7d7e861931feb
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-06-14 21:31:14 -0400

    Created detail view controller and removed default view controller

--- SprintChallenge/SprintChallenge/View Controllers/SKIPokemonDetailViewController.h
+++ SprintChallenge/SprintChallenge/View Controllers/SKIPokemonDetailViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  SKIPokemonDetailViewController.h
 //  SprintChallenge
 //
 //  Created by Joshua Rutkowski on 6/14/20.
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
 
+@interface SKIPokemonDetailViewController : UIViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
---
+++ SprintChallenge/SprintChallenge/View Controllers/SKIPokemonDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  SKIPokemonDetailViewController.m
+//  SprintChallenge
+//
+//  Created by Joshua Rutkowski on 6/14/20.
+//  Copyright © 2020 Ski. All rights reserved.
+//
+
+#import "SKIPokemonDetailViewController.h"
+
+@interface SKIPokemonDetailViewController ()
+
+@end
+
+@implementation SKIPokemonDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
