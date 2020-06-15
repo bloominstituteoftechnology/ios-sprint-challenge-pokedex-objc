0224ed437bfc3cf40ce57ff5d20eff4129f40d02
David Wright <davidtwright89@gmail.com>
2020-06-14 23:16:11 -0400

    Added a DTWPokemon model object

---
+++ Pokedex-Objc/Pokedex-Objc/Models/DTWPokemon.h
@@ -0,0 +1,17 @@
+//
+//  DTWPokemon.h
+//  Pokedex-Objc
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTWPokemon : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex-Objc/Pokedex-Objc/Models/DTWPokemon.m
@@ -0,0 +1,13 @@
+//
+//  DTWPokemon.m
+//  Pokedex-Objc
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "DTWPokemon.h"
+
+@implementation DTWPokemon
+
+@end
