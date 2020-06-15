a31690e04b6ab00b0c51328ac7a9a2eddbf3a461
David Wright <davidtwright89@gmail.com>
2020-06-15 00:26:41 -0400

    Added a DTWPokemonResults model object

---
+++ Pokedex-Objc/Pokedex-Objc/Models/DTWPokemonResults.m
@@ -0,0 +1,13 @@
+//
+//  DTWPokemonResults.m
+//  Pokedex-Objc
+//
+//  Created by David Wright on 6/15/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "DTWPokemonResults.h"
+
+@implementation DTWPokemonResults
+
+@end
