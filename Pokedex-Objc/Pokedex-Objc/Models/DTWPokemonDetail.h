020792ca9328d2563df7dd9d12768b40d1996028
David Wright <davidtwright89@gmail.com>
2020-06-14 23:17:09 -0400

    Added a DTWPokemonDetail model object

---
+++ Pokedex-Objc/Pokedex-Objc/Models/DTWPokemonDetail.h
@@ -0,0 +1,17 @@
+//
+//  DTWPokemonDetail.h
+//  Pokedex-Objc
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTWPokemonDetail : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
