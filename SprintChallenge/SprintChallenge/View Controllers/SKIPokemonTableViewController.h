43bdf49f47b13d5640dfc57d2780558dd368c319
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-06-14 21:30:25 -0400

    Created tableView controller and fixed entry point for storyboard

---
+++ SprintChallenge/SprintChallenge/View Controllers/SKIPokemonTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  SKIPokemonTableViewController.h
+//  SprintChallenge
+//
+//  Created by Joshua Rutkowski on 6/14/20.
+//  Copyright © 2020 Ski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SKIPokemonTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
