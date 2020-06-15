744a63dc4be588def7abcb29c83b56a7c8de87ff
David Wright <davidtwright89@gmail.com>
2020-06-14 23:14:05 -0400

    Added a DTWPokemonTableViewController class

---
+++ Pokedex-Objc/Pokedex-Objc/View Controllers/DTWPokemonTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  DTWPokemonTableViewController.h
+//  Pokedex-Objc
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTWPokemonTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
