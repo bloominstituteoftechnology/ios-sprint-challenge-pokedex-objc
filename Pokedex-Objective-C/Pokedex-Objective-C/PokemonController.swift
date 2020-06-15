f27cda11690bedeeced6034378d4585011146936
Alex Shillingford <alex.shillingford@icloud.com>
2020-06-14 21:47:42 -0600

    Add in fetch and fillInDetails method for PokemonController and fill out Ability to populate table view controller. Table View now populates with pokemon

--- Pokedex-Objective-C/Pokedex-Objective-C/PokemonController.swift
+++ Pokedex-Objective-C/Pokedex-Objective-C/PokemonController.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+class WASPokemonController: NSObject {
+    @objc(sharedController) static let shared = WASPokemonController()
+    
+    private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    
+    @objc func fetchAllPokemon(completion: @escaping ([WASPokemon]?, Error?) -> Void) {
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        urlComponents?.queryItems = [URLQueryItem(name: "limit", value: "100")]
+        guard let fetchURL = urlComponents?.url else { return }
+        
+       let dataTask = URLSession.shared.dataTask(with: fetchURL) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching data: \(error)")
+                completion(nil, error)
+                return
+            }
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                completion(nil, error)
+                return
+            }
+            do {
+                guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else { throw NSError() }
+                guard let results = json["results"] as? [[String: Any]] else { throw NSError() }
+                let allPokemon = results.map({ WASPokemon(dictionary: $0) })
+                completion(allPokemon, nil)
+            } catch {
+                NSLog("Unable to serialize json data: \(error)")
+                return completion(nil, error)
+            }
+        }
+        dataTask.resume()
+    }
+    
+    @objc func fillInDetails(for pokemon: WASPokemon) {
+        let url = baseURL.appendingPathComponent(pokemon.name)
+        //NSLog("Retrieving details for \(pokemon.name) with URL: \(url)")
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching data: \(error)")
+                return
+            }
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                return
+            }
+            do {
+                guard let jsonDictionary = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else { throw NSError() }
+                let detailPokemon = WASPokemon(dictionary: jsonDictionary)
+                pokemon.name = detailPokemon.name
+                pokemon.frontImage = detailPokemon.frontImage
+                pokemon.identifier = detailPokemon.identifier
+                pokemon.abilities = detailPokemon.abilities
+            } catch {
+                NSLog("Unable to serialize json data: \(error)")
+                return
+            }
+        }
+        dataTask.resume()
+    }
+}
--- Pokedex-Objective-C/Pokedex-Objective-C/WASTableViewController.m
+++ Pokedex-Objective-C/Pokedex-Objective-C/WASTableViewController.m
@@ -7,87 +7,62 @@
 //
 
 #import "WASTableViewController.h"
+#import "WASPokemon.h"
+#import "Pokedex_Objective_C-Swift.h"
 
 @interface WASTableViewController ()
+
+@property (nonatomic) NSArray<WASPokemon *> *allPokemon;
 
 @end
 
 @implementation WASTableViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self loadPokedex];
+}
+
+- (void)loadPokedex
+{
+    [[WASPokemonController sharedController] fetchAllPokemonWithCompletion:^(NSArray<WASPokemon *> *fetchedPokemon, NSError *error) {
+        if (error) {
+            NSLog(@"Error fetching names in TableViewController's `loadPokedex` method: %@", error);
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.allPokemon = fetchedPokemon;
+            [self.tableView reloadData];
+        });
+    }];
 }
 
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.allPokemon.count;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
-}
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+ {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PokeCell" forIndexPath:indexPath];
     
-    // Configure the cell...
+     cell.textLabel.text = self.allPokemon[indexPath.row].name;
     
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+ {
+     
 }
-*/
+
 
 @end
