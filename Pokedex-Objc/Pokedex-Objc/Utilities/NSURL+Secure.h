555f67cca31384b5538cd74c5e849f8b365b364f
David Wright <davidtwright89@gmail.com>
2020-06-14 23:31:40 -0400

    Added a category extension named NSURL+Secure

---
+++ Pokedex-Objc/Pokedex-Objc/Utilities/NSURL+Secure.h
@@ -0,0 +1,17 @@
+//
+//  NSURL+Secure.h
+//  Pokedex-Objc
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSURL (Secure)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex-Objc/Pokedex-Objc/Utilities/NSURL+Secure.m
@@ -0,0 +1,13 @@
+//
+//  NSURL+Secure.m
+//  Pokedex-Objc
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "NSURL+Secure.h"
+
+@implementation NSURL (Secure)
+
+@end
