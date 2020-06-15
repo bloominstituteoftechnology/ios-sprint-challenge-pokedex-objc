68a974cc57d54bf2646a78991a646d309ad28366
David Wright <davidtwright89@gmail.com>
2020-06-15 05:38:14 -0400

    Declared the public interface for the DTWPokemonDetail model class

--- Pokedex-Objc/Pokedex-Objc/Models/DTWPokemonDetail.h
+++ Pokedex-Objc/Pokedex-Objc/Models/DTWPokemonDetail.h
@@ -8,10 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
+NS_SWIFT_NAME(PokemonDetail)
 @interface DTWPokemonDetail : NSObject
+
+@property (nonatomic, copy, readonly, nonnull) NSString *name;
+@property (nonatomic, readonly, nonnull) NSNumber *identifier;
+@property (nonatomic, readonly, nullable) NSURL *spriteURL;
+@property (nonatomic, copy, readonly, nonnull) NSString *abilities;
+
+- (nonnull instancetype)initWithName:(nonnull NSString *)name
+                          identifier:(nonnull NSNumber *)identifier
+                           spriteURL:(nullable NSURL *)spriteURL
+                           abilities:(nonnull NSString *)abilities;
+
+- (nullable instancetype)initWithDictionary:(nonnull NSDictionary *)dictionary;
 
 @end
 
-NS_ASSUME_NONNULL_END
