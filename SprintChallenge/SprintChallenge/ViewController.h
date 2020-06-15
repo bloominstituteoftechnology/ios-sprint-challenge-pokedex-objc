031b8f9d17ca5df30e1e7d4f86ece181bbecffd7
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-06-14 21:01:01 -0400

    Initial Commit

---
+++ SprintChallenge/SprintChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SprintChallenge
+//
+//  Created by Joshua Rutkowski on 6/14/20.
+//  Copyright © 2020 Ski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SprintChallenge/SprintChallenge/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SprintChallenge
+//
+//  Created by Joshua Rutkowski on 6/14/20.
+//  Copyright © 2020 Ski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
