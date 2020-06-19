de143995d6a54d08343e000e8bb712b804b25de1
Shawn James <shawn.james@me.com>
2020-06-19 11:08:45 -0400

    Add starter project

---
+++ ObjCPokedex/ObjCPokedex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjCPokedex
+//
+//  Created by Shawn James on 6/19/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObjCPokedex/ObjCPokedex/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ObjCPokedex
+//
+//  Created by Shawn James on 6/19/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
