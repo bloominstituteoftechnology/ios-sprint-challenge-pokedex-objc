a426983d6e8bea4895a544e419a80e0d059d883d
Chris Dobek <dobek.christopher@gmail.com>
2020-06-19 11:25:51 -0400

    Storyboard created with PokemonTableVC and PokemonDetailVC

---
+++ Pokedex/Pokedex/View Controllers/CMDPokemonDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  CMDPokemonDetailViewController.m
+//  Pokedex
+//
+//  Created by Chris Dobek on 6/19/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+#import "CMDPokemonDetailViewController.h"
+
+@interface CMDPokemonDetailViewController ()
+
+@end
+
+@implementation CMDPokemonDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- Pokedex/Pokedex/View Controllers/CMDPokemonTableViewController.h
+++ Pokedex/Pokedex/View Controllers/CMDPokemonTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CMDPokemonTableViewController.h
 //  Pokedex
 //
 //  Created by Chris Dobek on 6/19/20.
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
 
+@interface CMDPokemonTableViewController : UITableViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
