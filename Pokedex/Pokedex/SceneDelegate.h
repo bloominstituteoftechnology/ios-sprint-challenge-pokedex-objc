7bb15386efe23d31c6890843f45ca022548b7706
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-19 08:42:15 -0700

    Updated .gitignore & added project

---
+++ Pokedex/Pokedex/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Pokedex
+//
+//  Created by Bhawnish Kumar on 6/19/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Pokedex/Pokedex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pokedex
+//
+//  Created by Bhawnish Kumar on 6/19/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
