9433de91f95d1bd312bdec88e3b64bb42b9d1f2e
Shawn James <shawn.james@me.com>
2020-06-19 11:33:05 -0400

    Add Pokemon object file

---
+++ ObjCPokedex/ObjCPokedex/Model/SDJPokemon.h
@@ -0,0 +1,17 @@
+//
+//  SDJPokemon.h
+//  ObjCPokedex
+//
+//  Created by Shawn James on 6/19/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SDJPokemon : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjCPokedex/ObjCPokedex/Model/SDJPokemon.m
@@ -0,0 +1,13 @@
+//
+//  SDJPokemon.m
+//  ObjCPokedex
+//
+//  Created by Shawn James on 6/19/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+#import "SDJPokemon.h"
+
+@implementation SDJPokemon
+
+@end
