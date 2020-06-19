9d055f9db78eb3e1f93875291f834c223a90eb35
Patrick Millet <lohs79seahawk@gmail.com>
2020-06-19 10:44:44 -0500

    Created Xcode Project, moved starter files into resources and changed location of info.plist in build settings. Created some groups to handle MVC separation.

---
+++ Objc-Pokedex/Objc-Pokedex/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Objc-Pokedex
+//
+//  Created by Patrick Millet on 6/19/20.
+//  Copyright © 2020 PatrickMillet79. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Objc-Pokedex/Objc-Pokedex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objc-Pokedex
+//
+//  Created by Patrick Millet on 6/19/20.
+//  Copyright © 2020 PatrickMillet79. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
