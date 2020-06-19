b130d5e9c87e5dbf845ed92165a78262af614649
Shawn James <shawn.james@me.com>
2020-06-19 12:05:56 -0400

    Implement the UI updates that should happen when information has returned

--- ObjCPokedex/ObjCPokedex/ViewController/DetailVC.swift
+++ ObjCPokedex/ObjCPokedex/ViewController/DetailVC.swift
@@ -30,6 +30,31 @@ class DetailVC: UIViewController {
         pokemon.addObserver(self, forKeyPath: "image", options: .new, context: .none)
     }
     
+    /// Uses the KVO observers to update the UI of whatever has returned
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        guard let pokemon = pokemon else { return }
+        switch keyPath {
+        case "identifier":
+            DispatchQueue.main.async {
+                self.idLabel.text = String(self.pokemon!.identifier)
+            }
+        case "abilities":
+            DispatchQueue.main.async {
+                let pokemonAbilities = pokemon.abilities as! [String]
+                self.abilitiesLabel.text = pokemonAbilities.joined(separator: ", ")
+            }
+        case "image":
+            self.networkController?.loadImage(urlString: pokemon.image, completion: { (pokemonImage, error) in
+                DispatchQueue.main.async {
+                    self.imageView.image = pokemonImage
+                }
+            })
+        default:
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+    
     /// Updates the views with the properties of the injected pokemon object
     private func updateViews() {
         guard let pokemon = pokemon else { return }
