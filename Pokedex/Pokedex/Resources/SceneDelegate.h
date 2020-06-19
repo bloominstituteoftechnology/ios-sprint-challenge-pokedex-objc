aa53f34b68789948cafadfd106b25becd9a2bff6
Chris Dobek <dobek.christopher@gmail.com>
2020-06-19 11:13:12 -0400

    Pokedex Project created

---
+++ Pokedex/Pokedex/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Pokedex
+//
+//  Created by Chris Dobek on 6/19/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
