9f4b2526af90a611758a560fb7e3b1bf2e712769
Ezra Black <dev.ezrablack@gmail.com>
2020-06-19 15:18:01 -0500

    Added Detail View Files

---
+++ Dex/Dex/DetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.h
+//  Dex
+//
+//  Created by Ezra Black on 6/19/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Dex/Dex/DetailsViewController.m
@@ -0,0 +1,32 @@
+//
+//  DetailsViewController.m
+//  Dex
+//
+//  Created by Ezra Black on 6/19/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
