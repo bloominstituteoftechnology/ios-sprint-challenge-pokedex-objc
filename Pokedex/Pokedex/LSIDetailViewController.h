415915ccf04ad1b01fb838aa0494762df11a43df
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-19 10:04:46 -0700

    Added the table view & detail view controllerS

---
+++ Pokedex/Pokedex/LSIDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  LSIDetailViewController.h
+//  Pokedex
+//
+//  Created by Bhawnish Kumar on 6/19/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/Pokedex/LSIDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  LSIDetailViewController.m
+//  Pokedex
+//
+//  Created by Bhawnish Kumar on 6/19/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import "LSIDetailViewController.h"
+
+@interface LSIDetailViewController ()
+
+@end
+
+@implementation LSIDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
