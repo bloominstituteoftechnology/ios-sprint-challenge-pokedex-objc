d5e7f494935b740a9754efe989bb516d8edb0b19
Shawn James <shawn.james@me.com>
2020-06-19 11:24:07 -0400

    Add tableView reusable cell id to the detailVC file for cellForRowAt

--- ObjCPokedex/ObjCPokedex/ViewController/DetailVC.swift
+++ ObjCPokedex/ObjCPokedex/ViewController/DetailVC.swift
@@ -20,7 +20,7 @@ class DetailVC: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath)
         return cell
     }
 
