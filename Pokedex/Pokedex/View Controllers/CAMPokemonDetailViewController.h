56145b6a7b6f2095a334f22a67f6950da36d73ac
Cody Morley <codymorleyinc@gmail.com>
2020-07-24 08:49:31 -0700

    View Controllers
    Created view controllers, connected classes to storyboards

---
+++ Pokedex/Pokedex/View Controllers/CAMPokemonDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  CAMPokemonDetailViewController.h
+//  Pokedex
+//
+//  Created by Cody Morley on 7/24/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CAMPokemonDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/Pokedex/View Controllers/CAMPokemonDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  CAMPokemonDetailViewController.m
+//  Pokedex
+//
+//  Created by Cody Morley on 7/24/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+#import "CAMPokemonDetailViewController.h"
+
+@interface CAMPokemonDetailViewController ()
+
+@end
+
+@implementation CAMPokemonDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
