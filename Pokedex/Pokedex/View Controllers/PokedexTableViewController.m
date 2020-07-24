e8af3a7b70d54dd1d360813e77eb1e43227dd154
Joe <squirlyproductions@gmail.com>
2020-07-24 12:38:40 -0400

    tableView work. working through detail VC.

--- Pokedex/Pokedex/View Controllers/PokedexTableViewController.m
+++ Pokedex/Pokedex/View Controllers/PokedexTableViewController.m
@@ -48,43 +48,57 @@
     _apiClient = [[PokemonAPIClient alloc] init];
 }
 
-
-
-
-
-
+//MARK: -Lifecylce
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
+    [self.apiClient fetchSortedPokemonWithCompletion:^(NSArray<Pokemon *> * _Nullable pokemon, NSError * _Nullable error) {
+        
+        if (error)
+            
+        {
+            NSLog(@"%@", error);
+        }
+        
+        else if (pokemon)
+            
+        {
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                self.pokemon = pokemon;
+                
+                [self.tableView reloadData];
+            });
+        }
+    }];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 
-    return 0;
+    return self.pokemon.count;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PokemonCell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    Pokemon *pokemon = self.pokemon[indexPath.row];
+    cell.textLabel.text = [pokemon.name capitalizedString];
     
     return cell;
 }
 
-
-
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
    
 }
--- Pokedex/Pokedex/View Controllers/PokemonDetailViewController.swift
+++ Pokedex/Pokedex/View Controllers/PokemonDetailViewController.swift
@@ -9,22 +9,23 @@
 import UIKit
 
 class PokemonDetailViewController: UIViewController {
+    
+    //MARK: Public Props
+    
+    @objc var pokemon: Pokemon?
+    @objc var apiClient: PokemonAPIClient?
+    
+    //MARK: Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: IBoutlets
+    
+    
+    // TODO: KVO
+    deinit {
+        pokemon?.removeObserver(self, forKeyPath: "abilities", context: &PokemonDetailViewController.kvoContext)
     }
-    */
-
 }
