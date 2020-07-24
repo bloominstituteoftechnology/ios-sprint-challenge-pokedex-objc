0db8acbe8ec11c700cef05cccaa4bf3967f71d85
Cody Morley <codymorleyinc@gmail.com>
2020-07-24 15:21:25 -0700

    PokedexTableViewController.m
    finished implementing tableview

--- Pokedex/Pokedex/View Controllers/CAMPokemonDetailViewController.m
+++ Pokedex/Pokedex/View Controllers/CAMPokemonDetailViewController.m
@@ -13,20 +13,12 @@
 @end
 
 @implementation CAMPokemonDetailViewController
-
+//MARK: - Life Cycles -
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    _pokedex = PokedexAPIController.shared;
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
