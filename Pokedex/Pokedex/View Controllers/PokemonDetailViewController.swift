31ff76a67a91e2713efc503f0cacb641ff82c66d
Joe <squirlyproductions@gmail.com>
2020-07-24 12:46:21 -0400

    added updateViews methods in detail VC. tackle KVO next

--- Pokedex/Pokedex/View Controllers/PokemonDetailViewController.swift
+++ Pokedex/Pokedex/View Controllers/PokemonDetailViewController.swift
@@ -23,9 +23,41 @@ class PokemonDetailViewController: UIViewController {
     
     //MARK: IBoutlets
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
     
     // TODO: KVO
     deinit {
         pokemon?.removeObserver(self, forKeyPath: "abilities", context: &PokemonDetailViewController.kvoContext)
     }
+    
+    private static var kvoContext = 0
+    
+    private func updateSprite() {
+        
+        guard let pokemon = pokemon,
+            let spriteURL = pokemon.spriteURL,
+            let apiClient = apiClient else { return }
+        
+        apiClient.fetchPokeImage(url: spriteURL) { (result) in
+            
+            switch result {
+                
+            case .failure(let error):
+                
+                print(error)
+                
+            case .success(let image):
+                
+                DispatchQueue.main.async {
+                    
+                    self.imageView.image = image
+                }
+            }
+        }
+    }
+    
+    
 }
