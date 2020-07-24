557e440260e443254e862be2600dbf3d7a22170f
Cody Morley <codymorleyinc@gmail.com>
2020-07-24 15:02:03 -0700

    CAMPokedexTableViewController.h && CAMPokemonDetailViewController.h
    Set up header files

--- Pokedex/Pokedex/View Controllers/CAMPokedexTableViewController.h
+++ Pokedex/Pokedex/View Controllers/CAMPokedexTableViewController.h
@@ -7,10 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Pokedex-Swift.h"
+
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CAMPokedexTableViewController : UITableViewController
+
+@property PokedexAPIController *pokedex;
 
 @end
 
--- Pokedex/Pokedex/View Controllers/CAMPokemonDetailViewController.h
+++ Pokedex/Pokedex/View Controllers/CAMPokemonDetailViewController.h
@@ -7,10 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Pokedex-Swift.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CAMPokemonDetailViewController : UIViewController
+
+@property (nonatomic) int pokemonID;
+@property (nonatomic) PokedexAPIController *pokedex;
 
 @end
 
