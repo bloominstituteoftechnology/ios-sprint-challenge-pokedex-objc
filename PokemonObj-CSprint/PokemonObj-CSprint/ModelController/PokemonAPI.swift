b321207f6e4b83fbf1bff048a740a5104e4fe5f6
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-07-24 08:57:10 -0700

    PokemonAPI Complete

--- PokemonObj-CSprint/PokemonObj-CSprint/ModelController/PokemonAPI.swift
+++ PokemonObj-CSprint/PokemonObj-CSprint/ModelController/PokemonAPI.swift
@@ -7,3 +7,68 @@
 //
 
 import Foundation
+
+@objc class PokemonAPI: NSObject {
+    @objc (sharedController) static let shared = PokemonAPI()
+    
+    @objc func fetchAllPokemon(completion: @escaping ([EJSPokemon]?, Error?) -> Void) {
+        
+        let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon/")!
+        
+        URLSession.shared.dataTask(with: baseURL) { (data, _, error) in
+            
+            if let error = error {
+                completion(nil, error)
+                print("Error: \(error)")
+                return
+            }
+            guard let data = data else {
+                completion(nil,NSError())
+                print("no data")
+                return
+            }
+            do {
+                let dictionary = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+                let results = dictionary["results"] as! [[String: Any]]
+                let pokemonResults = results.map {EJSPokemon(dictionary: $0)}
+            } catch {
+                print("Decoding Error: \(error)")
+                completion(nil, NSError())
+                return
+            }
+        }.resume()
+    }
+    
+   @objc func fillInDetails(for pokemon: EJSPokemon) {
+                
+        var baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon/")!
+        baseURL = baseURL.appendingPathComponent(pokemon.name)
+                
+        URLSession.shared.dataTask(with: baseURL) { (data, _, error) in
+            
+            if let error = error {
+                print("ERROR: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                print("no data")
+                return
+            }
+            
+            do {
+                let dictionary = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let fetchedPokemon = EJSPokemon(dictionary: dictionary)
+                
+                pokemon.name = fetchedPokemon.name
+                pokemon.identifier = fetchedPokemon.identifier
+                pokemon.sprite = fetchedPokemon.sprite
+                pokemon.abilities = fetchedPokemon.abilities
+                                
+            } catch {
+                print("DECODE error: \(error)")
+                return
+            }
+        }.resume()
+    }
+}
