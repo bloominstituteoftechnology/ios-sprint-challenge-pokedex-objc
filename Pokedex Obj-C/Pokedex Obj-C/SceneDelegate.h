0917b74ec47a048013eacb37630bf4ada4d0ad00
nonye <nnezekwo@gmail.com>
2020-07-24 08:14:54 -0700

    Xcode Project created

---
+++ Pokedex Obj-C/Pokedex Obj-C/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Pokedex Obj-C
+//
+//  Created by Nonye on 7/24/20.
+//  Copyright © 2020 Nonye Ezekwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Pokedex Obj-C/Pokedex Obj-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pokedex Obj-C
+//
+//  Created by Nonye on 7/24/20.
+//  Copyright © 2020 Nonye Ezekwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
