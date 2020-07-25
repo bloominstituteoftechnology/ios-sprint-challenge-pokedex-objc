b41446d782b43d7cec9c1c52ea1870117408f7ff
Harmony Radley <harmonyradley99@gmail.com>
2020-07-24 20:10:01 -0400

    not finished

--- PokedexObjC/PokedexObjC/View Controllers/HMRDetailViewController.h
+++ PokedexObjC/PokedexObjC/View Controllers/HMRDetailViewController.h
@@ -6,11 +6,17 @@
 //  Copyright © 2020 Harmony Radley. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "HMRPokemon.h"
+#import "PokedexObjC-Swift.h"
+
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface HMRDetailViewController : NSObject
+
+@property (nonatomic, assign) PokemonController *pokedexController;
+@property (nonatomic, assign) HMRPokemon *pokemon;
 
 @end
 
--- PokedexObjC/PokedexObjC/View Controllers/HMRDetailViewController.m
+++ PokedexObjC/PokedexObjC/View Controllers/HMRDetailViewController.m
@@ -8,6 +8,30 @@
 
 #import "HMRDetailViewController.h"
 
+void *KVOContext = &KVOContext;
+
+@interface HMRDetailViewController ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UILabel *abilitiesLabel;
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *idLabel;
+
+@end
+
+
 @implementation HMRDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [self.pokedexController fillInDetailsFor:self.pokemon];
+    [self.pokemon addObserver:self forKeyPath:@"identifier" options:0 context:KVOContext];
+    [self.pokemon addObserver:self forKeyPath:@"image" options:0 context:KVOContext];
+    [self.pokemon addObserver:self forKeyPath:@"abilities" options:0 context:KVOContext];
+
+
+}
+
 
 @end
