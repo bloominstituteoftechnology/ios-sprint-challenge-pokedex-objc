2c10052a8024f89f3b83c3a944405620be8984f9
Chad Parker <chad@chadparker.com>
2020-08-14 20:14:31 -0700

    Add LSIPokemonResults

---
+++ Pokemon-Objc/Pokemon-Objc/Model/LSIPokemonResults.h
@@ -0,0 +1,17 @@
+//
+//  LSIPokemonResults.h
+//  Pokemon-Objc
+//
+//  Created by Chad Parker on 8/14/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIPokemonResults : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokemon-Objc/Pokemon-Objc/Model/LSIPokemonResults.m
@@ -0,0 +1,13 @@
+//
+//  LSIPokemonResults.m
+//  Pokemon-Objc
+//
+//  Created by Chad Parker on 8/14/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import "LSIPokemonResults.h"
+
+@implementation LSIPokemonResults
+
+@end
