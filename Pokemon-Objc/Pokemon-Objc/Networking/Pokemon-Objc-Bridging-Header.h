95f3a50cc33e6bec7056d6b9365757cd94385582
Chad Parker <chad@chadparker.com>
2020-08-14 20:55:33 -0700

    Implement PokemonAPI.fetchAllPokemon()

--- Pokemon-Objc/Pokemon-Objc/Networking/Pokemon-Objc-Bridging-Header.h
+++ Pokemon-Objc/Pokemon-Objc/Networking/Pokemon-Objc-Bridging-Header.h
@@ -3,3 +3,4 @@
 //
 
 #import "LSIPokemon.h"
+#import "LSIPokemonResults.h"
--- Pokemon-Objc/Pokemon-Objc/Networking/PokemonAPI.swift
+++ Pokemon-Objc/Pokemon-Objc/Networking/PokemonAPI.swift
@@ -8,14 +8,38 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    case noData
+    case noDecode
+}
+
 class PokemonAPI: NSObject {
+
+    private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon/")!
 
     @objc(sharedController) static let shared = PokemonAPI()
 
     private override init() {}
 
     @objc func fetchAllPokemon(completion: @escaping ([LSIPokemon]?, Error?) -> Void) {
-
+        URLSession.shared.dataTask(with: baseURL) { data, _, error in
+            guard error == nil else {
+                print("Error fetching data: \(error!)")
+                completion(nil, error!)
+                return
+            }
+            guard let data = data else {
+                print("Error: no data returned")
+                completion(nil, NetworkError.noData)
+                return
+            }
+            guard let results = LSIPokemonResults(from: data) else {
+                print("No pokemon results")
+                completion(nil, NetworkError.noDecode)
+                return
+            }
+            completion(results.pokemonArray, nil)
+        }.resume()
     }
 
     @objc func fillInDetails(for pokemon: LSIPokemon) {
