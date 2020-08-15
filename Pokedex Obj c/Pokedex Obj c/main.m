790dd21ac4da311a62dd76d5a9dc603f911563a9
Kevin Stewart <kistewart419@gmail.com>
2020-08-14 21:13:09 -0400

    Initial Commit: Set up basic storyboard, disabled ARC, and set prefix to KIS

---
+++ Pokedex Obj c/Pokedex Obj c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pokedex Obj c
+//
+//  Created by Kevin Stewart on 8/14/20.
+//  Copyright © 2020 Kevin Stewart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
