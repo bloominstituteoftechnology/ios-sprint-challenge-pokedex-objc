2e53a2eca98bfc5ad0680e6836fbd452fb98109f
Chad Parker <chad@chadparker.com>
2020-08-14 19:28:03 -0700

    Add PokemonAPI network controller

---
+++ Pokemon-Objc/Pokemon-Objc/Networking/PokemonAPI.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonAPI.swift
+//  Pokemon-Objc
+//
+//  Created by Chad Parker on 8/14/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import Foundation
+
+class PokemonAPI: NSObject {
+
+    @objc(sharedController) static let shared = PokemonAPI()
+
+    private override init() {}
+
+    @objc func fetchAllPokemon(completion: @escaping ([LSIPokemon]?, Error?) -> Void)
+
+    @objc func fillInDetails(for pokemon: LSIPokemon)
+}
