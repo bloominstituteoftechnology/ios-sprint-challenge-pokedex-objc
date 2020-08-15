4758b17bc8f26c1bbc7a54977a7a6da7f141990e
Claudia Maciel <clc_80@yahoo.com>
2020-08-14 18:05:00 -0700

    Added Xcode project

---
+++ Pokedex/Pokedex/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pokedex
+//
+//  Created by Claudia Maciel on 8/14/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Pokedex/Pokedex/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Pokedex
+//
+//  Created by Claudia Maciel on 8/14/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
