3f7b236b49a6271f15d9efd645d24a4331dda682
Chad Parker <chad@chadparker.com>
2020-08-14 20:58:56 -0700

    Add PokemonTableVC

---
+++ Pokemon-Objc/Pokemon-Objc/PokemonTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  PokemonTableViewController.h
+//  Pokemon-Objc
+//
+//  Created by Chad Parker on 8/14/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PokemonTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
