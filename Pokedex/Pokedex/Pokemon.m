c6fa0ca3987693182ad0ce0f56609d89d481c513
Claudia Maciel <clc_80@yahoo.com>
2020-08-14 18:37:32 -0700

    Added Pokemon model class

---
+++ Pokedex/Pokedex/Pokemon.m
@@ -0,0 +1,13 @@
+//
+//  Pokemon.m
+//  Pokedex
+//
+//  Created by Claudia Maciel on 8/14/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import "Pokemon.h"
+
+@implementation Pokemon
+
+@end
