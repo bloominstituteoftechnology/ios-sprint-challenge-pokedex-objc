036098bd0385f6bedc0a32063ec666ffd89fab56
Claudia Maciel <clc_80@yahoo.com>
2020-08-14 19:05:59 -0700

    Added Pokemon Results class

---
+++ Pokedex/Pokedex/PokemonResults.h
@@ -0,0 +1,17 @@
+//
+//  PokemonResults.h
+//  Pokedex
+//
+//  Created by Claudia Maciel on 8/14/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PokemonResults : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/Pokedex/PokemonResults.m
@@ -0,0 +1,13 @@
+//
+//  PokemonResults.m
+//  Pokedex
+//
+//  Created by Claudia Maciel on 8/14/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import "PokemonResults.h"
+
+@implementation PokemonResults
+
+@end
