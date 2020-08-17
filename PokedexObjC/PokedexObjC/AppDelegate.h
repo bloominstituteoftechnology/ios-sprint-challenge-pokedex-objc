7f00d00038c1d230c0d5fa454ef06e7eeb9fd245
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-16 21:20:27 -0400

    Created initial project

---
+++ PokedexObjC/PokedexObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PokedexObjC
+//
+//  Created by Sean Acres on 8/16/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PokedexObjC/PokedexObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PokedexObjC
+//
+//  Created by Sean Acres on 8/16/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
