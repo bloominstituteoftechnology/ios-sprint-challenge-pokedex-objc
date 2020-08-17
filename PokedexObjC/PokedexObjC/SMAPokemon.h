9b071ce1ae7415c3f5bc86f89d7a1b9f6862a0cb
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-16 21:28:28 -0400

    Added model files

---
+++ PokedexObjC/PokedexObjC/SMAPokemon.h
@@ -0,0 +1,17 @@
+//
+//  SMAPokemon.h
+//  PokedexObjC
+//
+//  Created by Sean Acres on 8/16/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SMAPokemon : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PokedexObjC/PokedexObjC/SMAPokemon.m
@@ -0,0 +1,13 @@
+//
+//  SMAPokemon.m
+//  PokedexObjC
+//
+//  Created by Sean Acres on 8/16/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import "SMAPokemon.h"
+
+@implementation SMAPokemon
+
+@end
