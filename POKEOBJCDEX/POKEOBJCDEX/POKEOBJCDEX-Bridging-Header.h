c8e7291f27be1d6406bb64eaf4ea09f3fa353720
Kelson Hartle <kelson99hartle@icloud.com>
2020-08-16 20:08:40 -0600

    Created bridging header and implemented API controller using a swift file

---
+++ POKEOBJCDEX/POKEOBJCDEX/POKEOBJCDEX-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+#import "KRHPokemon.h"
+//#import "KRHPokemonNameAndURL.h"
+//#import "KRHPokemonNameResults.h"
+#import "PokemonResults.h"
