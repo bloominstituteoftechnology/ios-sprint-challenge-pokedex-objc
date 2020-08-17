0a60d3e65d3bd0a972edabe0808ecf4d5ba30c63
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-16 22:14:35 -0400

    Completed fetchAllPokemon and set up tableviewcontroller

--- PokedexObjC/PokedexObjC/PokemonController.swift
+++ PokedexObjC/PokedexObjC/PokemonController.swift
@@ -10,11 +10,61 @@ import Foundation
 
 
 class PokemonController: NSObject {
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
+    
+    let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
 
     @objc(sharedController) static let shared = PokemonController()
 
     @objc func fetchAllPokemon(completion: @escaping ([SMAPokemon]?, Error?) -> Void) {
+        var request = URLRequest(url: baseURL)
+        request.httpMethod = HTTPMethod.get.rawValue
         
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                NSLog("error fetching all pokemon: \(error)")
+                completion(nil, error)
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                NSLog("bad response while fetching all pokemon: \(response)")
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from fetching all pokemon")
+                completion(nil, nil)
+                return
+            }
+            
+            guard let dictionary = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions(rawValue: 0)) as? [String : Any] else {
+                NSLog("error converting json to dictionary")
+                completion(nil, nil)
+                return
+            }
+            
+            guard let resultsArray = dictionary["results"] as? Array<[String: String]> else {
+                NSLog("error getting results array")
+                completion(nil, nil)
+                return
+            }
+            
+            var allPokemon = [SMAPokemon]()
+            for item in resultsArray {
+                allPokemon.append(SMAPokemon(dictionary: item))
+            }
+            
+            completion(allPokemon, nil)
+            
+        }.resume()
     }
 
     @objc func fillInDetails(for pokemon: SMAPokemon) {
--- PokedexObjC/PokedexObjC/PokemonTableViewController.m
+++ PokedexObjC/PokedexObjC/PokemonTableViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "PokemonTableViewController.h"
+#import "SMAPokemon.h"
+#import "PokedexObjC-Swift.h"
 
 @interface PokemonTableViewController ()
+
+@property NSArray<SMAPokemon *> *allPokemon;
 
 @end
 
@@ -17,34 +21,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [PokemonController.sharedController fetchAllPokemonWithCompletion:^(NSArray<SMAPokemon *> *pokemon, NSError *error) {
+        if (error) {
+            NSLog(@"error returning all pokemon");
+            return;
+        }
+
+        self.allPokemon = pokemon;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+    }];
 }
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.allPokemon.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PokemonCell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    SMAPokemon *pokemon = [self.allPokemon objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = pokemon.name;
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
