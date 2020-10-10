1eed6ca32fe001c9b8608cf69f856d99cf83fb12
Clayton Watkins <watkinclayton17@yahoo.com>
2020-10-09 18:27:28 -0700

    Set up interface properties for model object

--- PokedexObjcSprint/PokedexObjcSprint/Model/EFSPokemon.h
+++ PokedexObjcSprint/PokedexObjcSprint/Model/EFSPokemon.h
@@ -5,14 +5,20 @@
 //  Created by Clayton Watkins on 10/9/20.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface EFSPokemon : NSObject
 
 @property (nonatomic, copy, readonly, nonnull) NSString *name;
+@property (nonatomic, readwrite)NSNumber *identifier;
+@property (nonatomic, readwrite)UIImage *sprite;
+@property (nonatomic, readwrite, copy)NSArray *abilities;
 
+- (instancetype)initWithName:(NSString *)aName;
+
+- (nullable instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
 @end
 
