1f4e64b38d93cc56246f95ff18178018ebd8a461
Bronson <bronsonmullens@icloud.com>
2020-10-09 19:33:15 -0700

    Set up table and detail VCs with custom launch screen and button

---
+++ Pokedex/Pokedex/View Controllers/BRMPokemonDetailViewController.m
@@ -0,0 +1,31 @@
+//
+//  BRMPokemonDetailViewController.m
+//  Pokedex
+//
+//  Created by Bronson Mullens on 10/9/20.
+//
+
+#import "BRMPokemonDetailViewController.h"
+
+@interface BRMPokemonDetailViewController ()
+
+@end
+
+@implementation BRMPokemonDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Pokedex/Pokedex/View Controllers/BRMPokemonTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  BRMPokemonTableViewController.h
+//  Pokedex
+//
+//  Created by Bronson Mullens on 10/9/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BRMPokemonTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
