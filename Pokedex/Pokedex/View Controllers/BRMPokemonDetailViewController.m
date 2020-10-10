e8c8ae21bb8b2681641348f1b6f1c2b6e20664ce
Bronson <bronsonmullens@icloud.com>
2020-10-09 19:45:54 -0700

    Fixed issue with abilities label

--- Pokedex/Pokedex/View Controllers/BRMPokemonDetailViewController.m
+++ Pokedex/Pokedex/View Controllers/BRMPokemonDetailViewController.m
@@ -5,9 +5,17 @@
 //  Created by Bronson Mullens on 10/9/20.
 //
 
+#import "Pokedex-Swift.h"
 #import "BRMPokemonDetailViewController.h"
+#import "BRMPokemon.h"
+
+void *KVOContext = &KVOContext;
 
 @interface BRMPokemonDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *pokemonSpriteView;
+@property (weak, nonatomic) IBOutlet UILabel *pokeonIDLabel;
+@property (weak, nonatomic) IBOutlet UILabel *pokemonAbilitiesLabel;
 
 @end
 
@@ -15,17 +23,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [PokemonAPI.sharedController addObserver:self forKeyPath:@"pokemon" options:NSKeyValueObservingOptionInitial context:KVOContext];
+    [PokemonAPI.sharedController addObserver:self forKeyPath:@"pokemonSprite" options:NSKeyValueObservingOptionInitial context:KVOContext];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:YES];
+    
+    [PokemonAPI.sharedController removeObserver:self forKeyPath:@"pokemon" context:KVOContext];
+    [PokemonAPI.sharedController removeObserver:self forKeyPath:@"pokemonSprite" context:KVOContext];
 }
-*/
+
+- (void)updateViews {
+    BRMPokemon *pokemon = PokemonAPI.sharedController.pokemon;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.title = pokemon.name.capitalizedString;
+        self.pokeonIDLabel.text = [NSString stringWithFormat:@"ID: %i", pokemon.identifier];
+        self.pokemonAbilitiesLabel.text = [NSString stringWithFormat:@"Abilities:\n%@", pokemon.abilities.capitalizedString];
+        self.pokemonSpriteView.image = PokemonAPI.sharedController.pokemonSprite;
+    });
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary<NSKeyValueChangeKey,id> *)change
+                       context:(void *)context {
+    if (context == KVOContext) {
+        if ([keyPath isEqualToString:@"pokemon"]) {
+            [self updateViews];
+        } else if ([keyPath isEqualToString:@"pokemonSprite"]) {
+            [self updateViews];
+        } else {
+            [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+        }
+    }
+}
+
+- (void)dealloc {
+    PokemonAPI.sharedController.pokemon = nil;
+    PokemonAPI.sharedController.pokemonSprite = nil;
+}
+
+- (IBAction)onlineButtonTapped:(id)sender {
+    BRMPokemon *pokemon = PokemonAPI.sharedController.pokemon;
+    NSURL *url = [NSURL URLWithString:@"https://pokemondb.net/pokedex/"];
+    NSURL *fullURL = [url URLByAppendingPathComponent:pokemon.name];
+    
+    UIApplication *application = [UIApplication sharedApplication];
+    [application openURL:fullURL options:@{} completionHandler:nil];
+}
 
 @end
--- Pokedex/Pokedex/View Controllers/BRMPokemonTableViewController.m
+++ Pokedex/Pokedex/View Controllers/BRMPokemonTableViewController.m
@@ -6,8 +6,12 @@
 //
 
 #import "BRMPokemonTableViewController.h"
+#import "BRMPokemon.h"
+#import "Pokedex-Swift.h"
 
 @interface BRMPokemonTableViewController ()
+
+@property NSArray<BRMPokemon *> *pokedex;
 
 @end
 
@@ -16,77 +20,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self loadPokedex];
 }
 
-#pragma mark - Table view data source
+// MARK: - Methods
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+- (void)loadPokedex {
+    [PokemonAPI.sharedController fetchAllPokemonWithCompletion:^(NSArray<BRMPokemon *> *pokemonNames, NSError *error) {
+        if (error) {
+            NSLog(@"%@", error);
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.pokedex = pokemonNames;
+            [self.tableView reloadData];
+        });
+    }];
 }
+
+// MARK: - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.pokedex.count;
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PokemonCell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    cell.textLabel.text = [self.pokedex[indexPath.row].name capitalizedString];
     
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
+// MARK: - Navigation
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+    BRMPokemon *pokemon = self.pokedex[indexPath.row];
+    
+    [PokemonAPI.sharedController fillInDetailsFor:pokemon];
 }
-*/
 
 @end
