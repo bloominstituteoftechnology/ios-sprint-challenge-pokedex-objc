6b99ffcaacfe80e89ab63fc446ef54c58a89331b
Bronson <bronsonmullens@icloud.com>
2020-10-09 18:17:41 -0700

    Initial commit

---
+++ Pokedex/Pokedex/Resources/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Pokedex
+//
+//  Created by Bronson Mullens on 10/9/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
