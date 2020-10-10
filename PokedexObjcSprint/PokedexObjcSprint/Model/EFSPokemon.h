84c17b8e22a856226a5f37326ac9f29676119947
Clayton Watkins <watkinclayton17@yahoo.com>
2020-10-09 18:20:26 -0700

    Storyboard has been set up

---
+++ PokedexObjcSprint/PokedexObjcSprint/Model/EFSPokemon.h
@@ -0,0 +1,19 @@
+//
+//  EFSPokemon.h
+//  PokedexObjcSprint
+//
+//  Created by Clayton Watkins on 10/9/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EFSPokemon : NSObject
+
+@property (nonatomic, copy, readonly, nonnull) NSString *name;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PokedexObjcSprint/PokedexObjcSprint/Model/EFSPokemon.m
@@ -0,0 +1,12 @@
+//
+//  EFSPokemon.m
+//  PokedexObjcSprint
+//
+//  Created by Clayton Watkins on 10/9/20.
+//
+
+#import "EFSPokemon.h"
+
+@implementation EFSPokemon
+
+@end
