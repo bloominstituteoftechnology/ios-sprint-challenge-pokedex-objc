a03d1d5e21dd31ca54cc38e605d0e0df819feb59
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-12-12 09:48:36 -0800

    Project Setup and Storyboard

---
+++ Pokedex/Pokedex/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Pokedex
+//
+//  Created by Cora Jacobson on 12/12/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
