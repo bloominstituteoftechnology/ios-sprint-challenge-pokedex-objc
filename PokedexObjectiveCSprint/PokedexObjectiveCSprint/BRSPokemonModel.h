67d69d80c32287aa97eac03d004ac2ae321e8ebc
brys <brys1saclaus2@gmail.com>
2020-12-12 11:40:06 -0800

    added properties for model object

--- PokedexObjectiveCSprint/PokedexObjectiveCSprint/BRSPokemonModel.h
+++ PokedexObjectiveCSprint/PokedexObjectiveCSprint/BRSPokemonModel.h
@@ -5,12 +5,27 @@
 //  Created by BrysonSaclausa on 12/12/20.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface BRSPokemonModel : NSObject
 
+@property (nonatomic) NSString *name;
+@property (nonatomic) NSString *identifier;
+@property (nonatomic) UIImage *sprite;
+@property (nonatomic) NSArray *abilities;
+
+- (instancetype)initWithName:(NSString *)aName;
+
 @end
 
 NS_ASSUME_NONNULL_END
+
+
+/*
+ name
+ id
+ sprite
+ abilities
+ */
