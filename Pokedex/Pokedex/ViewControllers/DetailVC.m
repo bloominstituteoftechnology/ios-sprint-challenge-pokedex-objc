5cb0d64d617ebc1c135e4faad54570d0ef06e8a5
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-12-12 09:59:33 -0800

    Basic View Controllers

---
+++ Pokedex/Pokedex/ViewControllers/DetailVC.m
@@ -0,0 +1,26 @@
+//
+//  DetailVC.m
+//  Pokedex
+//
+//  Created by Cora Jacobson on 12/12/20.
+//
+
+#import "DetailVC.h"
+
+@interface DetailVC ()
+
+@property (nonatomic) IBOutlet UIImageView *imageView;
+@property (nonatomic) IBOutlet UILabel *nameLabel;
+@property (nonatomic) IBOutlet UILabel *identifierLabel;
+@property (nonatomic) IBOutlet UITextView *abilitiesTextView;
+
+@end
+
+@implementation DetailVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
---
+++ Pokedex/Pokedex/ViewControllers/PokedexTableVC.m
@@ -0,0 +1,42 @@
+//
+//  PokedexTableVC.m
+//  Pokedex
+//
+//  Created by Cora Jacobson on 12/12/20.
+//
+
+#import "PokedexTableVC.h"
+
+@interface PokedexTableVC ()
+
+@end
+
+@implementation PokedexTableVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"pokemonCell" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
