41fb0e66a8d631557429eb26a281f8b15bdedf69
Sammy Alvarado <isammyalvarado@gmail.com>
2020-12-14 14:27:17 -0800

    Initiated the VC
    Created the VC for the storyboard views.

---
+++ Pokedex-Sprint/Pokedex-Sprint/Model/SAEPokemonDetialViewController.h
@@ -0,0 +1,16 @@
+//
+//  SAEPokemonDetialViewController.h
+//  Pokedex-Sprint
+//
+//  Created by Sammy Alvarado on 12/14/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SAEPokemonDetialViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex-Sprint/Pokedex-Sprint/Model/SAEPokemonTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  SAEPokemonTableViewController.h
+//  Pokedex-Sprint
+//
+//  Created by Sammy Alvarado on 12/14/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SAEPokemonTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
