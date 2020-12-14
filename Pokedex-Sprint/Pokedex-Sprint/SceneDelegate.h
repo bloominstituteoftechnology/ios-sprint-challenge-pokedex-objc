edd382c112e3994d78cbf38c7528c36409e41d49
Sammy Alvarado <isammyalvarado@gmail.com>
2020-12-13 18:17:35 -0800

    Initial Project.
    Started the initial project.

---
+++ Pokedex-Sprint/Pokedex-Sprint/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Pokedex-Sprint
+//
+//  Created by Sammy Alvarado on 12/13/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
