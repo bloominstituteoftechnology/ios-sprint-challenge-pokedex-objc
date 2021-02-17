a4fe2bf58a7b8b3acae3c7d8568bd2ba52778c53
Kenneth Jones <kennethjones1991@outlook.com>
2021-02-17 16:21:49 -0700

    Created initial project

---
+++ PokedexObjc/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PokedexObjc
+//
+//  Created by Kenneth Jones on 2/17/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PokedexObjc/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  PokedexObjc
+//
+//  Created by Kenneth Jones on 2/17/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
