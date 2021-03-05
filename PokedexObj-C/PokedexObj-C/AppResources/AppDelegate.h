2a4ee9acb25b18f17eecc93391536d557fb0a135
James McDougall <jamesmcdougall@James-MBP.lan>
2021-03-05 16:03:33 -0600

    Initial Project Commit

---
+++ PokedexObj-C/PokedexObj-C/AppResources/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PokedexObj-C
+//
+//  Created by James McDougall on 3/5/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PokedexObj-C/PokedexObj-C/AppResources/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  PokedexObj-C
+//
+//  Created by James McDougall on 3/5/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
